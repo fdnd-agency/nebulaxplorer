f60f30081ddd901ceaf499a624b28306a7692cb4
Marcin Magdziak <marcinm@live.nl>
2025-10-16 12:42:53 +0200

    added imports & current page selection

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
-  import favicon from "$lib/assets/favicon.svg";
+  import { page } from "$app/stores";
+  import favicon from "$lib/assets/icons/favicon.svg";
+  import '$lib/assets/styles/general.css';
 
   let { children } = $props();
 </script>
@@ -7,7 +9,74 @@
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
+<header>
+  <nav>
+    <ul>
 
+      <ul>
+        <li>
+          <a
+            href="/"
+            aria-current={$page.url.pathname === "/" ? "page" : undefined}
+            >home</a
+          >
+        </li>
+        <li>
+          <a
+            href="/mission"
+            aria-current={$page.url.pathname === "/mission"
+              ? "page"
+              : undefined}>mission</a
+          >
+        </li>
+        <li>
+          <a
+            href="/scientific"
+            aria-current={$page.url.pathname === "/scientific"
+              ? "page"
+              : undefined}>scientific</a
+          >
+        </li>
+        <li>
+          <a
+            href="/news"
+            aria-current={$page.url.pathname === "/news" ? "page" : undefined}
+            >news</a
+          >
+        </li>
+        <li>
+          <a
+            href="/team"
+            aria-current={$page.url.pathname === "/team" ? "page" : undefined}
+            >team</a
+          >
+        </li>
+        <li>
+          <a
+            href="/careers"
+            aria-current={$page.url.pathname === "/careers"
+              ? "page"
+              : undefined}>careers</a
+          >
+        </li>
+        <li>
+          <a
+            href="/partners"
+            aria-current={$page.url.pathname === "/partners"
+              ? "page"
+              : undefined}>partners</a
+          >
+        </li>
+      </ul>
+    </ul>
+  </nav>
+</header>
 <main>
   {@render children?.()}
 </main>
+
+<style>
+  header{
+    background: var(--cleanroom-100);
+  }
+</style>
