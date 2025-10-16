111c097f147b20e9e7effe838f39585e5c89d93f
Marcin Magdziak <marcinm@live.nl>
2025-10-16 13:35:45 +0200

    basic setup
    - layout with some functions for path detection
    - arrow specifically for navigation (might just replace for the original)
    - layout.css for styling .main-navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
 <script>
   import { page } from "$app/stores";
   import favicon from "$lib/assets/icons/favicon.svg";
-  import '$lib/assets/styles/general.css';
+  import PageArrow from "$lib/assets/icons/PageArrow.svelte";
+  import "$lib/assets/styles/general.css";
+  import "$lib/assets/styles/layout.css";
+  
+
+  const isParentActive = (path) => {
+    return $page.url.pathname.startsWith(`${path}/`);
+  };
+
+  const isExactActive = (path) => {
+    return $page.url.pathname === path;
+  };
 
   let { children } = $props();
 </script>
@@ -9,74 +20,114 @@
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
-<header>
+
+<header class="main-navigation">
   <nav>
     <ul>
-
-      <ul>
-        <li>
-          <a
-            href="/"
-            aria-current={$page.url.pathname === "/" ? "page" : undefined}
-            >home</a
-          >
-        </li>
-        <li>
-          <a
-            href="/mission"
-            aria-current={$page.url.pathname === "/mission"
-              ? "page"
-              : undefined}>mission</a
-          >
-        </li>
-        <li>
-          <a
-            href="/scientific"
-            aria-current={$page.url.pathname === "/scientific"
-              ? "page"
-              : undefined}>scientific</a
-          >
-        </li>
-        <li>
-          <a
-            href="/news"
-            aria-current={$page.url.pathname === "/news" ? "page" : undefined}
-            >news</a
-          >
-        </li>
-        <li>
-          <a
-            href="/team"
-            aria-current={$page.url.pathname === "/team" ? "page" : undefined}
-            >team</a
-          >
-        </li>
-        <li>
-          <a
-            href="/careers"
-            aria-current={$page.url.pathname === "/careers"
-              ? "page"
-              : undefined}>careers</a
-          >
-        </li>
-        <li>
-          <a
-            href="/partners"
-            aria-current={$page.url.pathname === "/partners"
-              ? "page"
-              : undefined}>partners</a
-          >
-        </li>
-      </ul>
+      <li
+        class={isExactActive("/")
+          ? "exact-active"
+          : isParentActive("/")
+            ? "parent-active"
+            : ""}
+      >
+        <PageArrow />
+        <a href="/" aria-current={isExactActive("/") ? "page" : undefined}>
+          home
+        </a>
+      </li>
+      <li
+        class={isExactActive("/mission")
+          ? "exact-active"
+          : isParentActive("/mission")
+            ? "parent-active"
+            : ""}
+      >
+        <PageArrow />
+        <a
+          href="/mission"
+          aria-current={isExactActive("/mission") ? "page" : undefined}
+        >
+          mission
+        </a>
+      </li>
+      <li
+        class={isExactActive("/scientific")
+          ? "exact-active"
+          : isParentActive("/scientific")
+            ? "parent-active"
+            : ""}
+      >
+        <PageArrow />
+        <a
+          href="/scientific"
+          aria-current={isExactActive("/scientific") ? "page" : undefined}
+        >
+          scientific
+        </a>
+      </li>
+      <li
+        class={isExactActive("/news")
+          ? "exact-active"
+          : isParentActive("/news")
+            ? "parent-active"
+            : ""}
+      >
+        <PageArrow />
+        <a
+          href="/news"
+          aria-current={isExactActive("/news") ? "page" : undefined}
+        >
+          news
+        </a>
+      </li>
+      <li
+        class={isExactActive("/team")
+          ? "exact-active"
+          : isParentActive("/team")
+            ? "parent-active"
+            : ""}
+      >
+        <PageArrow />
+        <a
+          href="/team"
+          aria-current={isExactActive("/team") ? "page" : undefined}
+        >
+          team
+        </a>
+      </li>
+      <li
+        class={isExactActive("/careers")
+          ? "exact-active"
+          : isParentActive("/careers")
+            ? "parent-active"
+            : ""}
+      >
+        <PageArrow />
+        <a
+          href="/careers"
+          aria-current={isExactActive("/careers") ? "page" : undefined}
+          >careers</a
+        >
+      </li>
+      <li
+        class={isExactActive("/partners")
+          ? "exact-active"
+          : isParentActive("/careers")
+            ? "parent-active"
+            : ""}
+      >
+        <PageArrow />
+        <a
+          href="/partners"
+          aria-current={isExactActive("/partners") ? "page" : undefined}
+        >
+          partners
+        </a>
+      </li>
     </ul>
   </nav>
 </header>
 <main>
   {@render children?.()}
 </main>
-
-<style>
-  header{
-    background: var(--cleanroom-100);
-  }
-</style>
