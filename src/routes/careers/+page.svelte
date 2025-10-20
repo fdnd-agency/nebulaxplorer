2487a3a0a2c0aaa71fb39cacffc4456ba1ed2743
MarsGotBars <marcinm@live.nl>
2025-10-20 16:24:41 +0200

    added leading example for hero component

--- src/routes/careers/+page.svelte
+++ src/routes/careers/+page.svelte
@@ -1,5 +1,7 @@
 <script>
   import { Hero } from "$lib";
+  // ! NOTE: important to include ?enhanced, otherwise no optimization takes place nor does it get rendered !
+  import heroImg from "$lib/assets/images/grav-waves.jpg?enhanced"
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -7,4 +9,5 @@
   Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
   documentation
 </p>
-<Hero pageTitle="Careers" />
+<!-- Always provide alt and file path WITHIN background -->
+<Hero pageTitle="Careers" background={{ alt: "Two merging black holes", file: heroImg }} />
