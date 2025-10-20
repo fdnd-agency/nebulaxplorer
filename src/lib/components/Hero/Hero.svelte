59e55d4c8660e08376476a6790ad0cdc34df0b97
MarsGotBars <marcinm@live.nl>
2025-10-20 16:24:52 +0200

    changed defaults & some styling

--- src/lib/components/Hero/Hero.svelte
+++ src/lib/components/Hero/Hero.svelte
@@ -2,11 +2,11 @@
   let {
     background = {
       alt: "default background image",
-      path: "defpath",
+      file,
     },
     paragraph,
     sronIconPath = "defpath",
-    pageTitle = "None set"
+    pageTitle = "None set",
   } = $props();
 </script>
 
@@ -14,7 +14,34 @@
   {#if paragraph}
     <p>{paragraph}</p>
   {/if}
-  <img src={background.path} alt={background.alt} class="hero-bg" />
-  <img src={sronIconPath} alt="" class="hero-bg" />
+
+  <enhanced:img
+    src={background.file}
+    alt={background.alt}
+    class="hero-bg"
+    sizes="100vw"
+  />
+
   <h1>{pageTitle}</h1>
 </section>
+
+<style>
+  .hero {
+    position: relative;
+
+    :global(picture) {
+      display: contents;
+
+      enhanced\:img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center;
+        z-index: -1;
+      }
+    }
+  }
+</style>
