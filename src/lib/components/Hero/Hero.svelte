19440699334d557365a08d242a04e1cfd727d601
MarsGotBars <marcinm@live.nl>
2025-10-21 17:08:26 +0200

    check for icon and styling

--- src/lib/components/Hero/Hero.svelte
+++ src/lib/components/Hero/Hero.svelte
@@ -4,6 +4,7 @@
       alt: "default background image",
       file,
     },
+    sronIcon,
     paragraph,
     sronIconPath = "defpath",
     pageTitle = "None set",
@@ -22,13 +23,27 @@
     sizes="100vw"
   />
 
+  {#if sronIcon}
+    <img src={sronIcon} alt="Logo of SRON Academy" class="hero-logo" />
+  {/if}
   <h1>{pageTitle}</h1>
 </section>
 
 <style>
   .hero {
+    --menu-button-size: 3.75rem;
+    
     position: relative;
-    padding: 3.5rem 4rem 2.25rem 4rem;
+    padding: 1.5rem;
+    padding-top: 5.25rem;
+    @media (min-width: 56.25rem) {
+      
+      padding: 3.5rem 4rem 2.25rem 4rem;
+    }
+
+    > *:not(picture) + *:not(picture) {
+      margin-top: 1rem;
+    }
 
     :global(picture) {
       display: contents;
@@ -44,5 +59,12 @@
         z-index: -1;
       }
     }
+
+    .hero-logo{
+      width: clamp(10rem, calc(25vw + 5rem), 60rem);
+      max-width: 36.375rem;
+
+    }
+    border-bottom: .1875rem solid var(--cleanroom-100);
   }
 </style>
--- src/routes/careers/+page.svelte
+++ src/routes/careers/+page.svelte
@@ -2,7 +2,8 @@
   import { Hero } from "$lib";
   // ! NOTE: important to include ?enhanced, otherwise no optimization takes place nor does it get rendered !
   import heroImg from "$lib/assets/images/grav-waves.jpg?enhanced"
+  import logo from "$lib/assets/logos/SRON_Academy_OnSpaceBlue.svg"
 </script>
 
 <!-- Always provide alt and file path WITHIN background -->
-<Hero pageTitle="Careers" background={{ alt: "Two merging black holes", file: heroImg }} />
+<Hero pageTitle="Careers" background={{ alt: "Two merging black holes", file: heroImg }} sronIcon={logo} />
