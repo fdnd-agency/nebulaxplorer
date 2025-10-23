f71a17d1e63aed6bdfcae1451692ba0dfc4ea771
Halima Dinle <26465999+halie404@users.noreply.github.com>
2025-10-23 15:16:35 +0200

    created a component for the jobs list

---
+++ src/lib/components/jobs/Jobs.svelte
@@ -0,0 +1,28 @@
+<script>
+    const jobs = [
+        {
+            id: 1,
+            role: "Technical Engineer",
+            location: "Leiden",
+            slug: "",
+        },
+        {
+            id: 2,
+            role: "Astrophysics Data Analysis Intern",
+            location: "Leiden",
+            slug: "",
+        }
+    ];
+</script>
+
+<h1>Find a suitable assignment</h1>
+<p>Currently {jobs.length} available</p>
+
+<ul>
+    {#each jobs as job}
+        <li>
+            <h2>Role: {job.role}</h2>
+            <p>Location: {job.location}</p>
+        </li>
+    {/each}
+</ul>
