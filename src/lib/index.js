f3915732262e817dba91755b42cb39fa78e795a8
Halima Dinle <26465999+halie404@users.noreply.github.com>
2025-10-23 15:20:29 +0200

    added the jobs list to the vacany page

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,5 @@
 // Assets
 
 // General components
-export { default as Hero } from './components/Hero/Hero.svelte';
-
-
+export { default as Hero } from "./components/Hero/Hero.svelte";
+export { default as JobForm } from "./components/jobs/Jobs.svelte";
--- src/routes/careers/+page.svelte
+++ src/routes/careers/+page.svelte
@@ -3,7 +3,12 @@
   // ! NOTE: important to include ?enhanced, otherwise no optimization takes place nor does it get rendered !
   import heroImg from "$lib/assets/images/grav-waves.jpg?enhanced"
   import logo from "$lib/assets/logos/SRON_Academy_OnSpaceBlue.svg"
+  import Jobs from "$lib/components/jobs/Jobs.svelte";
 </script>
 
 <!-- Always provide alt and file path WITHIN background -->
 <Hero pageTitle="Careers" background={{ alt: "Two merging black holes", file: heroImg }} sronIcon={logo} />
+
+<section>
+    <Jobs />
+</section>
