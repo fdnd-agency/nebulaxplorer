c05479179cd87bc5e3a7b5c34b722799c4824af8
Halima Dinle <26465999+halie404@users.noreply.github.com>
2025-10-23 14:57:39 +0200

    mock data for displaying jobs temp solution

---
+++ src/lib/components/form/JobForm.svelte
@@ -0,0 +1,16 @@
+<script>
+    const jobs = [
+        {
+            "id": 1,
+            "role": "Technical engineer",
+            "location": "Leiden",
+            "slug": "",
+        },
+           {
+            "id": 2,
+            "role": "Astrophysics Data Analysis Intern",
+            "location": "Leiden",
+            "slug": "",
+        }
+    ];
+</script>
