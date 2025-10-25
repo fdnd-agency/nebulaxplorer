2ec56994bd9b4ae0dea8421a54ccc28009c386e3
Halima Dinle <26465999+halie404@users.noreply.github.com>
2025-10-25 17:17:34 +0200

    final changes v1 job component

--- src/lib/components/jobs/Jobs.svelte
+++ src/lib/components/jobs/Jobs.svelte
@@ -4,33 +4,122 @@
       id: 1,
       role: "Technical Engineer",
       location: "Leiden",
-      slug: "",
+      slug: ""
     },
     {
       id: 2,
       role: "Astrophysics Data Analysis Intern",
       location: "Leiden",
-      slug: "",
+      slug: ""
+    },
+    {
+      id: 3,
+      role: "Astrophysics Data Analysis Intern",
+      location: "Leiden",
+      slug: ""
+    },
+    {
+      id: 4,
+      role: "Astrophysics Data Analysis Intern",
+      location: "Leiden",
+      slug: ""
+    },
+    {
+      id: 5,
+      role: "Astrophysics Data Analysis Intern",
+      location: "Leiden",
+      slug: ""
+    },
+    {
+      id: 6,
+      role: "Astrophysics Data Analysis Intern",
+      location: "Leiden",
+      slug: ""
     },
   ];
 </script>
 
-<section class="assignment-header">
-  <h1 class="heading">Find a suitable <span>assignment</span></h1>
-  <p class="subheading">Currently <span>{jobs.length}</span> available</p> 
-</section>
+
+
+<ul class="jobs-grid">
+  {#each jobs as job}
+  <li class="job-card">
+    <div class="job-info">
+      <span class="job-label">role</span>
+      <h3>{job.role}</h3>
+      <span class="job-label">location</span>
+      <p>{job.location}</p>
+    </div>
+    <div class="job-image">
+      <img src="/src/lib/assets/images/WerkenbijSRON.jpg" alt="{job.role} in {job.location}"> 
+    </div>
+  </li>
+  {/each}
+</ul>
 
 <style>
-  .heading{
-    font-weight: 400;
+
+  .jobs-grid {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2rem;
+    padding: 2rem;
+    list-style: none;
+    justify-content: right;
   }
 
-  .subheading{
-    font-weight: 400;
+  .job-card {
+    width: 23.31rem;
+    height: 40rem;
+    background: var(--space-110);
+    overflow: hidden;
+    border-top: 3px solid var(--white);
+    border-bottom: 3px solid var(--white);
+    display: flex;
+    flex-direction: column;
   }
 
-  span {
-    font-weight: 500;
-    color: var( --cleanroom-100);
+  .job-info {
+    padding: 1.5rem 2rem 0 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+  h3 {
+    font-family: var(--heading);
+    font-weight: 400;
+    font-size: 1.3rem;
+    line-height: 1.75rem;
+    color: var(--white);
+  }
+  p { 
+    font-family: var(--heading);
+    font-weight: 400;
+    font-size: 1.2rem;
+    color: var(--white); 
+  }
+  }
+
+  .job-label {
+    font-family: var(--subtext);
+    font-weight: 700;
+    font-size: 1rem;
+    line-height: 1.4rem;
+    color: var(--cleanroom-100);
+    text-transform: lowercase;
+  }
+
+  .job-image {
+    width: 100%;
+    height: 28.125rem;
+    overflow: hidden;
+    margin-top: auto;
+  }
+
+  .job-image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
   }
 </style>
