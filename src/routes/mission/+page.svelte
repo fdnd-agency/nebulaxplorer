e2d83f0bcc65884fe0084b93be88d3950b5b2724
MarsGotBars <marcinm@live.nl>
2025-10-26 23:19:46 +0100

    adjusted html

--- src/routes/mission/+page.svelte
+++ src/routes/mission/+page.svelte
@@ -131,6 +131,50 @@
   background={{ alt: "", file: blackholeImage }}
 />
 
+<section class="paragraph-block">
+  <div class="text-title">
+    <h2 class="subtitle">What are we <span class="orange">up</span> to?</h2>
+    <p class="heading">What our current project entails</p>
+  </div>
+
+  <div class="text-content">
+    <h3 class="orange subtitle text-content--title">Mission brief</h3>
+    <h4 class="orange heading">Mission in development</h4>
+    <p>
+      NEBULA – Xplorer staat voor “Netherlands Educational Satellite for
+      Exploration of Binary-Linked Astrophysics – X-ray Observer”. Ongeveer
+      vierhonderd studenten helpen SRON, veertien Nederlandse
+      onderwijsinstellingen en vele industriële partners om deze ruimtemissie te
+      ontwikkelen van begin tot eind, onder leiding van wetenschappers en
+      ingenieurs.
+    </p>
+
+    <h4 class="orange heading">Questions surrounding X-ray binaries</h4>
+    <p>
+      Veel van de helderste objecten in het heelal zijn röntgendubbelsterren.
+      Dit zijn combinaties van een extreem compact object, zoals een zwart gat
+      of een neutronenster, en een begeleidende ster. Het compacte object
+      onttrekt in de loop van de tijd de materie van zijn begeleidende ster.
+      Hierbij komt veel energie vrij in een gebundelde straal—een jet.
+      Wetenschappers begrijpen dit proces is nog niet helemaal, en zeker de aard
+      van de stroom van materie vlak naast het zwarte gat blijft een groot
+      mysterie.
+    </p>
+
+    <h4 class="orange heading">Questions surrounding X-ray binaries</h4>
+    <p>
+      NEBULA-Xplorer gaat onderzoeken hoe jets vormen en hoe deze
+      röntgendubbelsterren evolueren. Hiervoor observeert hij deze objecten voor
+      lange periodes om te zien hoe de emissie van deze bronnen varieert op
+      tijdschalen van milliseconden tot weken. Deze lange observatietijd  maakt
+      het mogelijk om röntgenactiviteit te combineren met gegevens uit andere
+      golflengtes van andere telescopen.
+    </p>
+  </div>
+
+  <enhanced:img src={blackholeImage} alt="Black Hole" />
+</section>
+
 <style>
   .paragraph-block {
     display: grid;
