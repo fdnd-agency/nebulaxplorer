2c33241261117cab446bfae4ed9d1a18dc1a6c6e
Stella <stellaclough@gmail.com>
2025-10-27 18:47:07 +0100

    Converted mission paragraphs to components

--- src/routes/mission/+page.svelte
+++ src/routes/mission/+page.svelte
@@ -2,6 +2,7 @@
   import blackholeImage from "$lib/assets/blackhole.jpg?enhanced";
   import Hero from "$lib/components/Hero/Hero.svelte";
   import logo from "$lib/assets/logos/SRON_Academy_OnSpaceBlue.svg";
+  import Paragraph from "$lib/components/Paragraph/Paragraph.svelte";
 </script>
 
 <Hero
@@ -15,44 +16,11 @@
     <h2 class="subtitle">What are we <span class="orange">up</span> to?</h2>
     <p class="heading">What our current project entails</p>
   </div>
-
-  <div class="text-content">
-    <h3 class="orange subtitle text-content--title">Mission brief</h3>
-    <h4 class="orange heading">Mission in development</h4>
-    <p>
-      NEBULA – Xplorer staat voor “Netherlands Educational Satellite for
-      Exploration of Binary-Linked Astrophysics – X-ray Observer”. Ongeveer
-      vierhonderd studenten helpen SRON, veertien Nederlandse
-      onderwijsinstellingen en vele industriële partners om deze ruimtemissie te
-      ontwikkelen van begin tot eind, onder leiding van wetenschappers en
-      ingenieurs.
-    </p>
-
-    <h4 class="orange heading">Questions surrounding X-ray binaries</h4>
-    <p>
-      Veel van de helderste objecten in het heelal zijn röntgendubbelsterren.
-      Dit zijn combinaties van een extreem compact object, zoals een zwart gat
-      of een neutronenster, en een begeleidende ster. Het compacte object
-      onttrekt in de loop van de tijd de materie van zijn begeleidende ster.
-      Hierbij komt veel energie vrij in een gebundelde straal—een jet.
-      Wetenschappers begrijpen dit proces is nog niet helemaal, en zeker de aard
-      van de stroom van materie vlak naast het zwarte gat blijft een groot
-      mysterie.
-    </p>
-
-    <h4 class="orange heading">Questions surrounding X-ray binaries</h4>
-    <p>
-      NEBULA-Xplorer gaat onderzoeken hoe jets vormen en hoe deze
-      röntgendubbelsterren evolueren. Hiervoor observeert hij deze objecten voor
-      lange periodes om te zien hoe de emissie van deze bronnen varieert op
-      tijdschalen van milliseconden tot weken. Deze lange observatietijd  maakt
-      het mogelijk om röntgenactiviteit te combineren met gegevens uit andere
-      golflengtes van andere telescopen.
-    </p>
-  </div>
-
-  <enhanced:img src={blackholeImage} alt="Black Hole" />
+  
+  <img src={blackholeImage} alt="Black Hole" width="630px" />
 </section>
+
+<Paragraph />
 
 <style>
   .paragraph-block {
@@ -82,7 +50,7 @@
       }
     }
 
-    .text-content {
+    :global(.text-content) {
       * + p {
         margin-top: 0.5rem;
         @media (min-width: 56.25rem) {
@@ -90,18 +58,17 @@
         }
       }
 
-      p + *,
-      .text-content--title + * {
+      * + p {
         margin-top: 2rem;
       }
     }
 
-    .text-content,
-    picture {
+    :global(.text-content),
+    :global(picture) {
       margin-top: 3rem;
     }
 
-    picture {
+    :global(picture) {
       grid-column: 4 / -1;
       height: fit-content;
 
@@ -115,8 +82,8 @@
         grid-column: 8 / -1;
       }
 
-      enhanced\:img,
-      img {
+      :global(enhanced\:img),
+      :global(img) {
         width: 100%;
         object-fit: cover;
         max-height: 22.8125rem;
@@ -133,7 +100,7 @@
     color: var(--cleanroom-100);
   }
 
-  enhanced\:img {
+  :global(enhanced\:img) {
     width: auto;
   }
 </style>
