ef566c9c48537dbbd72a90bbf33d2f656f19d1a4
RenzoWille <renzo.wille@hva.nl>
2025-11-01 12:27:12 +0100

    #92 second paragraph finished

--- src/routes/mission/+page.svelte
+++ src/routes/mission/+page.svelte
@@ -61,43 +61,112 @@
 
 
 <section class="steps-block">
-    <h3 class="subtitle text-content--title">The <span class="orange">steps to</span> take before <span class="orange">launch</span></h3>
-    <div class="steps-content">
-      
-      <div class="step-item">
-        <span class="step-number">1</span>
-        <enhanced:img class="steps-img" src={blackholeImage} alt="Step 1 for launch" />
-      </div>
-
-      <div class="step-item">
-        <span class="step-number">2</span>
-        <enhanced:img class="steps-img" src={blackholeImage} alt="Step 2 for launch" />
-      </div>
-
-      <div class="step-item">
-        <span class="step-number">3</span>
-        <enhanced:img class="steps-img" src={blackholeImage} alt="Step 3 for launch" />
-      </div>
-      
-      <div class="step-item">
-        <span class="step-number">4</span>
-        <enhanced:img class="steps-img" src={blackholeImage} alt="Step 4 for launch" />
-      </div>
-
-      <div class="step-item">
-        <span class="step-number">5</span>
-        <enhanced:img class="steps-img" src={blackholeImage} alt="Step 5 for launch" />
-      </div>
-      
+  <h3 class="subtitle text-content--title">
+    The <span class="orange">steps to</span> take before
+    <span class="orange">launch</span>
+  </h3>
+  <div class="steps-content">
+    <div class="step-item">
+      <span class="step-number">1</span>
+      <enhanced:img
+        class="steps-img"
+        src={blackholeImage}
+        alt="Step 1 for launch"
+      />
     </div>
+
+    <div class="step-item">
+      <span class="step-number">2</span>
+      <enhanced:img
+        class="steps-img"
+        src={blackholeImage}
+        alt="Step 2 for launch"
+      />
+    </div>
+
+    <div class="step-item">
+      <span class="step-number">3</span>
+      <enhanced:img
+        class="steps-img"
+        src={blackholeImage}
+        alt="Step 3 for launch"
+      />
+    </div>
+
+    <div class="step-item">
+      <span class="step-number">4</span>
+      <enhanced:img
+        class="steps-img"
+        src={blackholeImage}
+        alt="Step 4 for launch"
+      />
+    </div>
+
+    <div class="step-item">
+      <span class="step-number">5</span>
+      <enhanced:img
+        class="steps-img"
+        src={blackholeImage}
+        alt="Step 5 for launch"
+      />
+    </div>
+  </div>
+</section>
+
+<section class="paragraph-block-right">
+  <enhanced:img class="paragraph-img-left" src={blackholeImage} alt="Black Hole" />
+  <section class="text-content-right">
+    <h3 class="orange subtitle text-content--title">Mission brief</h3>
+
+    <article>
+      <h4 class="orange heading">Mission in development</h4>
+      <p>
+        NEBULA – Xplorer staat voor “Netherlands Educational Satellite for
+        Exploration of Binary-Linked Astrophysics – X-ray Observer”. Ongeveer
+        vierhonderd studenten helpen SRON, veertien Nederlandse
+        onderwijsinstellingen en vele industriële partners om deze ruimtemissie
+        te ontwikkelen van begin tot eind, onder leiding van wetenschappers en
+        ingenieurs.
+      </p>
+    </article>
+
+    <article>
+      <h4 class="orange heading">Questions surrounding X-ray binaries</h4>
+      <p>
+        Veel van de helderste objecten in het heelal zijn röntgendubbelsterren.
+        Dit zijn combinaties van een extreem compact object, zoals een zwart gat
+        of een neutronenster, en een begeleidende ster. Het compacte object
+        onttrekt in de loop van de tijd de materie van zijn begeleidende ster.
+        Hierbij komt veel energie vrij in een gebundelde straal—een jet.
+        Wetenschappers begrijpen dit proces is nog niet helemaal, en zeker de
+        aard van de stroom van materie vlak naast het zwarte gat blijft een
+        groot mysterie.
+      </p>
+    </article>
+    <article>
+      <h4 class="orange heading">Questions surrounding X-ray binaries</h4>
+      <p>
+        NEBULA-Xplorer gaat onderzoeken hoe jets vormen en hoe deze
+        röntgendubbelsterren evolueren. Hiervoor observeert hij deze objecten
+        voor lange periodes om te zien hoe de emissie van deze bronnen varieert
+        op tijdschalen van milliseconden tot weken. Deze lange observatietijd
+         maakt het mogelijk om röntgenactiviteit te combineren met gegevens uit
+        andere golflengtes van andere telescopen.
+      </p>
+    </article>
+  </section>
 </section>
 
 <style>
+
+  /* FIRST PARAGRAPH BLOCK */
+
   .paragraph-block {
     display: grid;
     grid-template-columns: subgrid;
     gap: inherit;
     row-gap: unset;
+    margin-bottom: 1rem;
 
     > *:not(enhanced\:img, img, picture) {
       grid-column: 1 / -1;
@@ -161,83 +230,84 @@
     }
   }
 
-.steps-block {
+   /* STEPS BLOCK */
+
+  .steps-block {
     display: grid;
     grid-template-columns: subgrid;
     gap: inherit;
     row-gap: unset;
-    grid-column: 1 / -1; 
+    grid-column: 1 / -1;
     max-width: 100%;
     background-color: var(--white);
-}
+  }
 
-.text-content--title {
-  color: var(--space-100);
-  margin-top: 2rem;
-}
+  .text-content--title {
+    color: var(--space-100);
+    margin-top: 2rem;
+  }
 
-.steps-block .text-content--title {
-    grid-column: 1 / -1; 
+  .steps-block .text-content--title {
+    grid-column: 1 / -1;
     margin-bottom: 1.5rem;
-}
+  }
 
-.steps-content {
+  .steps-content {
     grid-column: 1 / -1;
     display: flex;
-    overflow-x: auto; 
-    scroll-snap-type: x mandatory; 
-    -webkit-overflow-scrolling: touch; 
-    margin-inline: calc(-.1 * 1.5rem );
+    overflow-x: auto;
+    scroll-snap-type: x mandatory;
+    -webkit-overflow-scrolling: touch;
+    margin-inline: calc(-0.1 * 1.5rem);
     padding-inline: 1.5rem;
     gap: 0;
-}
+  }
 
-.step-item {
+  .step-item {
     position: relative;
     flex: 0 0 80vw;
-    scroll-snap-align: start; 
-    margin-right: 1.5rem; 
-}
+    scroll-snap-align: start;
+    margin-right: 1.5rem;
+  }
 
-.step-item:last-child {
+  .step-item:last-child {
     margin-right: 0;
-}
+  }
 
-
-@media (min-width: 56.25rem) {
+  @media (min-width: 56.25rem) {
     .steps-content {
-        display: grid; 
-        grid-template-columns: repeat(5, 1fr);
-        margin-inline: 0;
-        padding-inline: 0;
-        gap: 1.5rem; 
-        overflow-x: visible;
-        scroll-snap-type: none;
+      display: grid;
+      grid-template-columns: repeat(5, 1fr);
+      margin-inline: 0;
+      padding-inline: 0;
+      gap: 1.5rem;
+      overflow-x: visible;
+      scroll-snap-type: none;
     }
-    
+
     .step-item {
-        flex: initial; 
-        margin-right: 0;
+      flex: initial;
+      margin-right: 0;
     }
-}
+  }
 
-.step-item {
+  .step-item {
     position: relative;
-}
+  }
 
-.steps-img {
-  width: 241px;
+  .steps-img {
+    width: 241px;
     height: 420px;
     object-fit: cover;
     border-radius: 8px;
     z-index: 1;
     margin-bottom: 3rem;
-}
+  }
 
-.step-number {
+  .step-number {
     position: absolute;
-    background-color: var(--cleanroom-100); 
-    color: var(--space-100); 
+    background-color: var(--cleanroom-100);
+    color: var(--space-100);
     font-family: var(--heading);
     display: flex;
     align-items: center;
@@ -247,16 +317,76 @@
     width: 3.5rem;
     height: 3.5rem;
     border-radius: 50%;
-    z-index: 10;
+    z-index: 5;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
-}
+  }
 
-.step-item :global(enhanced\:img) {
+  .step-item :global(enhanced\:img) {
     width: 100%;
-}
+  }
 
 
+ /* SECOND PARAGRAPH BLOCK */
 
+  .paragraph-block-right {
+    display: grid;
+    grid-template-columns: subgrid;
+    gap: inherit;
+    row-gap: unset;
+    margin-bottom: 2rem;
+
+    > *:not(enhanced\:img, img, picture) {
+      grid-column: 1 / -1;
+      max-width: 43.75rem;
+      padding-left: none;
+
+      @media (min-width: 36.25rem) {
+        grid-column: 1 / 4;
+        padding-right: 1.5rem;
+      }
+      @media (min-width: 56.25rem) {
+        padding-left: 2.5rem;
+        grid-column: 6 / 12;
+        align-items: right;
+      }
+    }
+
+    .text-content-right article {
+      * + p {
+        margin-top: 0.5rem;
+        @media (min-width: 56.25rem) {
+          padding-left: 1.5rem;
+        }
+      }
+    }
+
+    :global(.text-content),
+    :global(picture) {
+      margin-top: 3rem;
+    }
+
+    :global(picture) {
+      grid-column: 4 / -1;
+      height: fit-content;
+
+      display: none;
+
+      @media (min-width: 36.25rem) {
+        display: block;
+      }
+
+      @media (min-width: 56.25rem) {
+        grid-column: 1 / 6;
+      }
+
+      :global(enhanced\:img),
+      :global(img) {
+        width: 100%;
+        object-fit: cover;
+        max-height: 31.8rem;
+      }
+    }
+  }
 
   /* util classes */
   .orange {
