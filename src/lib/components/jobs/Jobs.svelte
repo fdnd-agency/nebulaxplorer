4ff90ea9251d860125a344707c7d2f2d42564ed7
Halima Dinle <26465999+halie404@users.noreply.github.com>
2025-11-02 21:36:20 +0100

    changes

--- src/lib/components/jobs/Jobs.svelte
+++ src/lib/components/jobs/Jobs.svelte
@@ -44,14 +44,14 @@
   {#each jobs as job}
   <li class="job-card">
     <div class="job-info">
-      <span class="job-label">role</span>
+      <span class="job-label role">role</span>
       <h3>{job.role}</h3>
-      <span class="job-label">location</span>
+      <span class="job-label location">location</span>
       <p>{job.location}</p>
     </div>
-    <div class="job-image">
+   <!-- <div class="job-image">
       <img src="/src/lib/assets/images/WerkenbijSRON.jpg" alt="{job.role} in {job.location}"> 
-    </div>
+    </div>  -->
   </li>
   {/each}
 </ul>
@@ -63,7 +63,84 @@
 
   
 
-.grid-wrapper{
+  .grid-wrapper{
+    display: grid;
+    grid-template-columns: subgrid;
+
+  .jobs-grid{
+    list-style: none;
+    display: grid;
+    grid-template-columns: subgrid;
+    grid-column: 4 / -1;
+    gap: 1rem;
+
+
+  }
+
+  }
+
+  .job-card{
+    grid-column: span 3;
+    background: var(--space-100-low-opacity);
+    border-top: 3px solid var(--white);
+    border-bottom: 3px solid var(--white);
+
+  @media (min-width: 56.25rem) {
+      background: inherit;
+      transform: translateX(0%);
+      position: initial;
+      width: initial;
+    }
+  }
+  .job-info{
+     padding: 2rem;
+    margin-bottom: 5rem;
+    gap: 0.5rem;
+
+     h3{
+    font-family: var(--heading);
+    font-weight: 400;
+    font-size: 1.3rem;
+    line-height: 1.75rem;
+    color: var(--white);
+   }
+   p{
+    font-family: var(--heading);
+    font-weight: 400;
+    font-size: 1.2rem;
+    color: var(--white);
+   }
+  }
+
+  .job-label {
+    font-family: var(--subtext);
+    font-weight: 700;
+    font-size: 1rem;
+    line-height: 1.4rem;
+    color: var(--cleanroom-100);
+ 
+  }
+
+  .job-label.location{
+    margin-top: 2rem;
+  }
+
+     .job-image {
+  width: 100%;
+    height: 28.125rem;
+    overflow: hidden;
+    margin-top: auto;
+    margin-bottom: auto;
+    img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: bottom;
+  }
+  }
+
+  
+/* .grid-wrapper{
   display: inherit;
   grid-template-columns: inherit;
 
@@ -78,14 +155,14 @@
 
 .job-card{
   grid-column: span 3;
-   background: var(--space-110);
+   background: var(--space-100-low-opacity);
     overflow: hidden;
     border-top: 3px solid var(--white);
     border-bottom: 3px solid var(--white);
 }
 
 .job-info{
-   padding: 1.5rem 2rem 0 2rem;
+   padding: 2rem;
        gap: 0.5rem;
    h3{
     font-family: var(--heading);
