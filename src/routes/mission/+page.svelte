f06b761edd89a9897983789b0d9426cc33c98a0c
RenzoWille <renzo.wille@hva.nl>
2025-11-15 15:35:08 +0100

    #110 set width and smaller height on smaller screens

--- src/routes/mission/+page.svelte
+++ src/routes/mission/+page.svelte
@@ -314,7 +314,6 @@
 
   .step-item {
     position: relative;
-    flex: 0 0 80vw;
     scroll-snap-align: start;
     margin-right: 1.5rem;
   }
@@ -346,11 +345,15 @@
 
   .steps-img {
     width: 15.063rem;
-    height: 26.25rem;
+    height: 20.5rem;
     object-fit: cover;
     border-radius: .5rem;
     z-index: 1;
     margin-bottom: 3rem;
+
+    @media (min-width: 56.25rem) {
+    height: 26.25rem;
+  }
   }
 
   .step-number {
@@ -369,7 +372,7 @@
   }
 
   .step-item :global(enhanced\:img) {
-    width: 100%;
+    width: 15.063rem;
   }
 
 
