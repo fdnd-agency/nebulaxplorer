5d463808f42471db8367fb8f6e7a2a3a10718c77
Marcin Magdziak <marcinm@live.nl>
2025-11-26 17:27:54 +0100

    rough pagination using the jobs arr
    this will be changed to correctly use the limits and be based on the amount of vacancies rather than being set by the amount of items in the job list

---
+++ src/lib/components/molecules/Pagination/Pagination.svelte
@@ -0,0 +1,61 @@
+<script>
+  import { slugifyString } from "$lib/utils/slugify";
+  import { page } from "$app/state";
+  import Arrow from "$lib/assets/icons/arrow.svelte";
+
+  const { limit = 9 } = $props();
+
+  const jobs = [
+    {
+      id: 1,
+      role: "Technical Engineer",
+      location: "Leiden",
+      slug: "",
+    },
+    {
+      id: 2,
+      role: "Astrophysics Data Analysis Intern",
+      location: "Leiden",
+      slug: "",
+    },
+    {
+      id: 3,
+      role: "Astrophysics Data Analysis Intern",
+      location: "Leiden",
+      slug: "",
+    },
+    {
+      id: 4,
+      role: "Astrophysics Data Analysis Intern",
+      location: "Leiden",
+      slug: "",
+    },
+    {
+      id: 5,
+      role: "Astrophysics Data Analysis Intern",
+      location: "Leiden",
+      slug: "",
+    },
+    {
+      id: 6,
+      role: "Astrophysics Data Analysis Intern",
+      location: "Leiden",
+      slug: "",
+    },
+  ];
+</script>
+
+<form action="GET">
+  {#each jobs as job, index}
+    <a href={`${page.url.pathname}?page=${index+1}`} class={`${page.url.pathname}`}>{index+1}</a>
+  {/each}
+</form>
+
+<style>
+    .page-indicator{
+        &.active{
+            text-decoration: underline;
+            font-weight: 500;
+        }
+    }
+</style>
