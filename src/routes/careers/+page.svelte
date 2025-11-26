dee978447f8300ead85d6ff188b25e0803cc7060
MarsGotBars <marcinm@live.nl>
2025-11-26 23:52:37 +0100

    replaced jobs with regular list of directus vacancies

--- src/routes/careers/+page.svelte
+++ src/routes/careers/+page.svelte
@@ -7,6 +7,10 @@
   // ! NOTE: important to include ?enhanced, otherwise no optimization takes place nor does it get rendered !
   import heroImg from "$lib/assets/images/grav-waves.jpg?enhanced";
   import logo from "$lib/assets/logos/SRON_Academy_OnSpaceBlue.svg";
+
+  const { data } = $props();
+  const { vacancies, pagination } = $derived(data);
+  
 </script>
 
 <!-- Always provide alt and file path WITHIN background -->
@@ -15,6 +19,13 @@
   background={{ alt: "Two merging black holes", file: heroImg }}
   sronIcon={logo}
 />
+
 <Breadcrumb />
-<Jobs />
-<Pagination />
+<!-- <Jobs /> -->
+<ul>
+  {#each vacancies as vacancy}
+    <li>{vacancy.title}</li>
+  {/each}
+</ul>
+
+<Pagination {pagination} />
