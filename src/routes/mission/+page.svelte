d9a8ceec75fce95c2bd283121d7d440dd0338e56
RenzoWille <renzo.wille@hva.nl>
2025-11-26 14:09:19 +0100

    svg change for supported technologies

--- src/routes/mission/+page.svelte
+++ src/routes/mission/+page.svelte
@@ -276,12 +276,16 @@
 <div class="testimonial-container">
   <div class="testimonial-content">
     <svg
-      class="quote-icon"
-      width="48"
-      height="48"
-      viewBox="0 0 48 48"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+    <svg
+    class="quote-icon"
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    focusable="false"
+  >
     >
       <path
         d="M12 28C12 24.686 14.686 22 18 22V18C12.477 18 8 22.477 8 28V34C8 36.209 9.791 38 12 38H18C20.209 38 22 36.209 22 34V28C22 25.791 20.209 24 18 24H12ZM32 28C32 24.686 34.686 22 38 22V18C32.477 18 28 22.477 28 28V34C28 36.209 29.791 38 32 38H38C40.209 38 42 36.209 42 34V28C42 25.791 40.209 24 38 24H32Z"
