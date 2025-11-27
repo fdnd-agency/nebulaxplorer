3da500142854f80183442725a979ff120e139a34
MarsGotBars <marcinm@live.nl>
2025-11-27 01:06:29 +0100

    wrapped in check for pagination

--- src/lib/components/molecules/Pagination/Pagination.svelte
+++ src/lib/components/molecules/Pagination/Pagination.svelte
@@ -45,70 +45,72 @@
   ];
 </script>
 
-<nav class="heading pagination-container {contrast}">
-  <!-- Previous button -->
-  {#if pagination.currentPage > 1}
-    <a
-      href={`${page.url.pathname}?page=${pagination.currentPage - 1}`}
-      class="pagination-indicator previous"
-      aria-label="Previous page"
-    >
-      <Arrow height="20" width="12" rotate="180deg" />
-    </a>
-  {:else}
-    <span class="pagination-indicator disabled previous" aria-hidden="true">
-      <Arrow height="20" width="12" rotate="180deg" />
-    </span>
-  {/if}
-
-  {#each pagination.items as item, index}
-    {#if item !== "-"}
+{#if pagination.totalPages > 1}
+  <nav class="heading pagination-container {contrast}">
+    <!-- Previous button -->
+    {#if pagination.currentPage > 1}
       <a
-        href={`${page.url.pathname}?page=${item}`}
-        class={`pagination-indicator ${pagination.currentPage === item ? "active" : ""}`}
+        href={`${page.url.pathname}?page=${pagination.currentPage - 1}`}
+        class="pagination-indicator previous"
+        aria-label="Previous page"
       >
-        {item}
+        <Arrow height="20" width="12" rotate="180deg" />
       </a>
     {:else}
-      <div class="separator-container">
-        <span class="pagination-separator">{item}</span>
-        <form
-          method="GET"
-          action=""
-          onsubmit={(e) => {
-            // Allow the GET navigation to happen
-            // but reset the input value immediately
-            setTimeout(() => e.target.reset());
-          }}
-          class="heading pagination-dictator"
-        >
-          <!-- The reason we have an empty placeholder here is for CSS styling, to check if the input is filled -->
-          <input
-            class="enhanced-navigation"
-            type="number"
-            name="page"
-            id="navigation-input-{index}"
-            min="1"
-            max={pagination.totalPages}
-            placeholder=""
-          />
-        </form>
-      </div>
+      <span class="pagination-indicator disabled previous" aria-hidden="true">
+        <Arrow height="20" width="12" rotate="180deg" />
+      </span>
     {/if}
-  {/each}
 
-  <!-- Next button -->
-  {#if pagination.currentPage < pagination.totalPages}
-    <a
-      href={`${page.url.pathname}?page=${pagination.currentPage + 1}`}
-      class="heading pagination-indicator"
-      aria-label="Next page"
-    >
-      <Arrow height="20" width="12" />
-    </a>
-  {:else}
-    <span class="heading page-indicator disabled" aria-hidden="true"
-      ><Arrow height="20" width="12" /></span
-    >
-  {/if}
-</nav>
+    {#each pagination.items as item, index}
+      {#if item !== "-"}
+        <a
+          href={`${page.url.pathname}?page=${item}`}
+          class={`pagination-indicator ${pagination.currentPage === item ? "active" : ""}`}
+        >
+          {item}
+        </a>
+      {:else}
+        <div class="separator-container">
+          <span class="pagination-separator">{item}</span>
+          <form
+            method="GET"
+            action=""
+            onsubmit={(e) => {
+              // Allow the GET navigation to happen
+              // but reset the input value immediately
+              setTimeout(() => e.target.reset());
+            }}
+            class="heading pagination-dictator"
+          >
+            <!-- The reason we have an empty placeholder here is for CSS styling, to check if the input is filled -->
+            <input
+              class="enhanced-navigation"
+              type="number"
+              name="page"
+              id="navigation-input-{index}"
+              min="1"
+              max={pagination.totalPages}
+              placeholder=""
+            />
+          </form>
+        </div>
+      {/if}
+    {/each}
+
+    <!-- Next button -->
+    {#if pagination.currentPage < pagination.totalPages}
+      <a
+        href={`${page.url.pathname}?page=${pagination.currentPage + 1}`}
+        class="heading pagination-indicator"
+        aria-label="Next page"
+      >
+        <Arrow height="20" width="12" />
+      </a>
+    {:else}
+      <span class="heading page-indicator disabled" aria-hidden="true"
+        ><Arrow height="20" width="12" /></span
+      >
+    {/if}
+  </nav>
+{/if}
