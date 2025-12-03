f154b7bb866580c6e4024e9565c4de0e009f7b12
Halima Dinle <26465999+halie404@users.noreply.github.com>
2025-12-03 12:11:03 +0100

    #169 news fetch

--- src/routes/news/+page.server.js
+++ src/routes/news/+page.server.js
@@ -1,24 +1,7 @@
-
-
 import { DIRECTUS_NEWS } from "$env/static/private";
 
-
-export async function load({ url }) {
-  const currentPage = Number(url.searchParams.get("page")) || 1;
-  const limit = 9;
-  const offset = (currentPage - 1) * limit;
-
-  const response = await fetch(
-    `${DIRECTUS_NEWS}?limit=${limit}&offset=${offset}&meta=total_count`
-  );
-
-  const json = await response.json();
-
-  const totalPages = Math.ceil(json.meta.total_count / limit);
-
-
-  return {
-    news: json.data,
-   
-  };
+export async function load({ fetch }) {
+  const response = await fetch(`${DIRECTUS_NEWS}`);
+  const { data } = await response.json();
+  return { news: data };
 }
