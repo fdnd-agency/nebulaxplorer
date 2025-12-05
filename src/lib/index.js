56fd7028e7879e2113b3e0a6435750a3d668fa35
Marcin Magdziak <marcinm@live.nl>
2025-12-05 13:55:25 +0100

    arrow import

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,5 +2,5 @@
 
 // General components
 export { default as Hero } from './components/molecules/Hero/Hero.svelte';
-
+export { default as Arrow } from './components/molecules/Breadcrumb/Breadcrumb.svelte'
 
