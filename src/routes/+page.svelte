5a36ca6024681892ce2ea981e3bdf1f3d07405f7
RenzoWille <renzo.wille@hva.nl>
2025-12-19 13:48:56 +0100

    #212 news temp removal pt.2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 
   import Mission from "$lib/components/atoms/Mission.svelte";
-  import NewsComponent from "$lib/components/News-Component/News-Component.svelte";
+  // import NewsComponent from "$lib/components/News-Component/News-Component.svelte";
 </script>
 
 <!-- <Mission {data} /> -->
