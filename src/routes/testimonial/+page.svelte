0b5546087c4a31af597301eb25eaaf61e9125962
RenzoWille <renzo.wille@hva.nl>
2025-12-19 13:20:41 +0100

    importation of hero

---
+++ src/routes/testimonial/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Hero from "$lib/components/molecules/Hero/Hero.svelte"
+    import heroImg from "$lib/assets/images/grav-waves.jpg?enhanced";
+
+
+    console.log(Hero)
+</script>
+
+<Hero
+  pageTitle="Careers"
+  background={{ alt: "Two merging black holes", file: heroImg }}
+  sronIcon={logo}
+/>
+
