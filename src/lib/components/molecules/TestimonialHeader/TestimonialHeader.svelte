907e0af744b10a1f4a431fc2c2a5b8e569bc0908
RenzoWille <renzo.wille@hva.nl>
2025-12-22 12:08:39 +0100

    more mock data + impact stats + import of components
    @halie404

---
+++ src/lib/components/molecules/TestimonialHeader/TestimonialHeader.svelte
@@ -0,0 +1,22 @@
+<header>
+    <h1>Impact of NEBULA-Xplorer</h1>
+    <div class="impact-stats">
+        <h3>~100</h3>
+        <span>Students involved</span>
+    </div>
+
+    <div class="impact-stats">
+        <h3>14</h3>
+        <span>Dutch universities involved</span>
+    </div>
+
+    <div class="impact-stats">
+        <h3>In development</h3>
+        <span>X-Ray instrument</span>
+    </div>
+
+    <div class="impact-stats">
+        <h3>1</h3>
+        <span>Satellite mission</span>
+    </div>
+</header>
