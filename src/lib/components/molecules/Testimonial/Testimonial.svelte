b2a5f8a33caa1fa42a46c836a7ecbb6ed67d5a8d
RenzoWille <renzo.wille@hva.nl>
2026-01-03 13:50:55 +0100

    #211 background change

--- src/lib/components/molecules/Testimonial/Testimonial.svelte
+++ src/lib/components/molecules/Testimonial/Testimonial.svelte
@@ -104,7 +104,7 @@
     }
 
     .testimonial-card {
-        background: var(--space-140);
+        background: var(--space-100);
         backdrop-filter: blur(0.625rem);
         border-radius: 0.5rem;
         padding: 1.5rem;
