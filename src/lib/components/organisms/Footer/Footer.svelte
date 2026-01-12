43b79c4738a04cc2fe2c9d86c09a5e31d370c0d1
MarsGotBars <marcinm@live.nl>
2026-01-12 17:58:10 +0100

    footer HTML
    this is still incomplete as it is missing the actual footer link and form

--- src/lib/components/organisms/Footer/Footer.svelte
+++ src/lib/components/organisms/Footer/Footer.svelte
@@ -2,3 +2,29 @@
   import leidenImg from "$lib/assets/images/sron-leiden.webp?enhanced";
   import groningenImg from "$lib/assets/images/sron-groningen.webp?enhanced";
 </script>
+
+<footer>
+  <ul class="locations">
+    <li>
+      <h2>Leiden</h2>
+      <enhanced:img src={leidenImg} alt="The SRON building in Leiden" />
+      <ul class="contact paragraph">
+        <li>Niels Bohrweg 4</li>
+        <li>2333 CA Leiden</li>
+        <li>The Netherlands</li>
+        <li>+31 (0)88 777 56 00</li>
+      </ul>
+    </li>
+    <li>
+      <h2>Groningen</h2>
+      <enhanced:img src={groningenImg} alt="The SRON building in Groningen" />
+      <ul class="contact paragraph">
+        <li>Landleven 12</li>
+        <li>2333 CA Leiden</li>
+        <li>The Netherlands</li>
+        <li>+31 (0)88 777 56 00</li>
+      </ul>
+    </li>
+  </ul>
+</footer>
+
