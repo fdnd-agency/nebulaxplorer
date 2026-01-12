efe08e4cccf95f9432bcda8e67383dbfc9333ffa
MarsGotBars <marcinm@live.nl>
2026-01-12 17:56:45 +0100

    border adjustment

--- src/lib/components/molecules/Hero/Hero.svelte
+++ src/lib/components/molecules/Hero/Hero.svelte
@@ -41,7 +41,7 @@
 <style>
   .hero {
     --menu-button-size: 3.75rem;
-    border-bottom: 0.1875rem solid var(--cleanroom-100);
+    border-bottom: .375rem solid var(--cleanroom-100);
     position: relative;
     padding: 1.5rem;
     padding-top: 5.25rem;
