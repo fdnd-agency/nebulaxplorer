4689841b476c20f89f03450b7b191560a1cfc76d
MarsGotBars <marcinm@live.nl>
2026-01-12 17:58:26 +0100

    styling

--- src/lib/components/organisms/Footer/Footer.svelte
+++ src/lib/components/organisms/Footer/Footer.svelte
@@ -28,3 +28,61 @@
   </ul>
 </footer>
 
+<style>
+  footer {
+    border-top: .375rem solid var(--cleanroom-100);
+    background: var(--space-100);
+    padding: 0;
+    position: relative;
+    ul {
+      background: var(--ultra-100);
+      list-style: none;
+      padding: 0;
+      display: flex;
+
+      &.locations {
+        gap: 1.5rem;
+        padding-left: 1.5rem;
+        li {
+          position: relative;
+          background: inherit;
+          width: 100%;
+          display: inline-block;
+
+          h2 {
+            background: inherit;
+            position: absolute;
+            top: 0;
+            left: 0;
+            transform: translateX(-100%) rotate(-90deg);
+            transform-origin: right top;
+            padding: 1rem 2rem;
+          }
+
+          picture {
+            display: contents;
+
+            enhanced\:img {
+              width: 100%;
+              height: auto;
+              display: block;
+              object-fit: cover;
+            }
+          }
+        }
+      }
+
+      &.contact {
+        flex-wrap: wrap;
+        li {
+          display: inline;
+          line-height: inherit;
+          width: fit-content;
+          &:not(:last-child) {
+            padding-right: 18px;
+          }
+        }
+      }
+    }
+  }
+</style>
