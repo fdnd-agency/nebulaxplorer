abb9b18fe743fe40d764429d45bddc46f4d878c7
𝖍𝖆𝖑𝖎𝖊 <26465999+halie404@users.noreply.github.com>
2026-01-19 20:13:03 +0100

    Revert "News bug fixes January 01-02"

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,32 +1,30 @@
 <!-- <script>
-  import { Hero } from "$lib";
   import blackholeImage from "$lib/assets/images/blackhole2.jpg?enhanced";
   import logo from "$lib/assets/logos/SRON_Academy_OnWhite.svg";
   import Breadcrumb from "$lib/components/Breadcrumb/Breadcrumb.svelte";
   import Newscards from "$lib/components/Newscards/Newscards.svelte";
 
 export let data;
-  export let data;
 </script>
+ 
 
-<title>News - SRON Academy</title>
 <Hero
   pageTitle="News"
   sronIcon={logo}
   background={{ alt: "", file: blackholeImage }}
 />
-
+ 
 <Breadcrumb 
   /> 
 
 <section class="paragraph-block">
   <div class="text-title">
     <h2 class="subtitle">Our latest <span class="orange">developments</span></h2>
-    <p class="heading">Currently <span class="orange">{data.news.length}</span> available.</p>
+    <p class="heading">Currently <span class="orange">{data.news.length}</span> available</p>
   </div>
   </section>
    
-<NewsCards news={data.news}
+<Newscards data={data}
   /> 
 
 <style>
