d5cf1bc00a5a7f79b97e868d223acd65410a0480
Marcin Magdziak <marcinm@live.nl>
2026-01-21 13:53:51 +0100

    some basic view transition
    this is not based on #246

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -98,3 +98,51 @@
 </main>
 <!-- For some reason, passing navItems directly did not pass the prop correctly -->
 <Footer {navItems} />
+
+<style>
+  @media (min-width: 56.25rem) {
+    .main-navigation {
+      view-transition-name: header;
+    }
+    .exact-active :global(svg) {
+      /* other existing rules */
+      view-transition-name: active-page;
+    }
+  }
+
+  @keyframes fade-in {
+    from {
+      opacity: 0;
+    }
+  }
+
+  @keyframes fade-out {
+    to {
+      opacity: 0;
+    }
+  }
+
+  @keyframes slide-from-right {
+    from {
+      transform: translateX(15%);
+    }
+  }
+
+  @keyframes slide-to-left {
+    to {
+      transform: translateX(-5%);
+    }
+  }
+
+  :root::view-transition-old(root) {
+    animation:
+      90ms cubic-bezier(0.4, 0, 1, 1) both fade-out,
+      300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-left;
+  }
+
+  :root::view-transition-new(root) {
+    animation:
+      210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
+      300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
+  }
+</style>
