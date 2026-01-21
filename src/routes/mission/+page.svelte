e6c0dbd06cbd7a12b218b64b78856ffe7b69f851
RenzoWille <renzo.wille@hva.nl>
2026-01-21 15:29:35 +0100

    #220 added object-fit: contain; to step 2 & 3 plus formatted document

--- src/routes/mission/+page.svelte
+++ src/routes/mission/+page.svelte
@@ -11,7 +11,6 @@
   import heroImg from "$lib/assets/images/grav-waves.jpg?enhanced&w=1024;768;512";
 </script>
 
-
 <!-- HERO BLOCK -->
 <Hero
   titleColor="var(--white)"
@@ -22,7 +21,6 @@
 
 <Breadcrumb />
 
-
 <!-- FIRST PARAGRAPH + IMG BLOCK -->
 
 <section class="paragraph-block">
@@ -30,7 +28,7 @@
     <h2 class="subtitle">What are we <span class="orange">up</span> to?</h2>
     <p class="heading">What our current project entails</p>
   </div>
- 
+
   <section class="text-content">
     <article>
       <h4 class="orange heading">Dense matter and compact objects</h4>
@@ -136,7 +134,6 @@
   <enhanced:img src={blackholeImage} alt="Black Hole" />
 </section>
 
-
 <!-- STEPS BLOCK -->
 
 <section class="steps-block">
@@ -163,9 +160,7 @@
             src={step1Image}
             alt="Step 1 for launch"
           />
-          <p class="steps-img-overlay">
-            Requirements and Design Review
-          </p>
+          <p class="steps-img-overlay">Requirements and Design Review</p>
         </label>
       </div>
     </li>
@@ -205,9 +200,7 @@
             src={step3Image}
             alt="Step 3 for launch"
           />
-          <p class="steps-img-overlay">
-            Engineering models development
-          </p>
+          <p class="steps-img-overlay">Engineering models development</p>
         </label>
       </div>
     </li>
@@ -226,19 +219,13 @@
             src={step4Image}
             alt="Step 4 for launch"
           />
-          <p class="steps-img-overlay">
-            Build and test of Flight Model 
-          </p>
+          <p class="steps-img-overlay">Build and test of Flight Model</p>
         </label>
       </div>
     </li>
 
     <li class="step-item">
-      <div
-        tabindex="0"
-        role="button"
-        aria-label="Step 5: Launch"
-      >
+      <div tabindex="0" role="button" aria-label="Step 5: Launch">
         <input type="checkbox" id="step-5" class="step-checkbox" />
         <label for="step-5" class="step-label">
           <span class="step-number caption">5</span>
@@ -247,9 +234,7 @@
             src={step5Image}
             alt="Step 5 for launch"
           />
-          <p class="steps-img-overlay">
-            Launch
-          </p>
+          <p class="steps-img-overlay">Launch</p>
         </label>
       </div>
     </li>
@@ -283,7 +268,6 @@
 </figure>
 
 <style>
-
   /* FIRST PARAGRAPH BLOCK */
 
   .paragraph-block {
@@ -438,7 +422,6 @@
       }
     }
 
-
     .text-content-3 article {
       * + p {
         margin-top: 0.5rem;
@@ -539,6 +522,11 @@
 
   .step-item > div[tabindex="0"]:focus .steps-img-overlay {
     opacity: 1;
+  }
+
+  #step-2 ~ .step-label .steps-img,
+  #step-3 ~ .step-label .steps-img {
+    object-fit: contain;
   }
 
   @media (min-width: 56.25rem) {
@@ -660,9 +648,8 @@
     box-shadow: 0 0.25rem 0.375rem rgba(0, 0, 0, 0.3);
   }
 
-
   /* GETTING INVOLVED */
-/* 
+  /* 
   .getting-involved {
     padding-left: 1.5rem;
     margin-bottom: 1.5rem;
@@ -673,7 +660,6 @@
       padding: 0 3rem 0 3rem;
     }
   } */
-
 
   /* TESTIMONIAL BLOCK */
 
@@ -763,16 +749,16 @@
   }
 
   h2, h3 {
-      font-weight: 600;
-      line-height: 1.5;
-    }
+    font-weight: 600;
+    line-height: 1.5;
+  }
 
   h4 {
     font-weight: 500;
     padding-top: 1.5rem;
   }
 
-    /* article + article {
+  /* article + article {
   margin-top: 2rem;
 } */
 </style>
