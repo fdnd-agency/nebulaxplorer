ba1d7a4fee08e4175c0b82bdda589efe24486178
Iris van Willigen <iris.van.willigen@gmail.com>
2026-02-09 15:47:06 +0100

    feat: add section with introduction text #281

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,47 @@
-<script>
+<section class="section-paragraph-picture">
+	<h2 class="section_title">Over Nebula Xplorer</h2>
+	<p class="paragraph">
+		NEBULA – Xplorer staat voor “Netherlands Educational Satellite for
+		Exploration of Binary-Linked Astrophysics – X-ray Observer”. Ongeveer
+		vierhonderd studenten helpen SRON, veertien Nederlandse
+		onderwijsinstellingen en vele industriële partners om deze ruimtemissie
+		te ontwikkelen van begin tot eind, onder leiding van wetenschappers en
+		ingenieurs. De missie heeft twee doelen. Het wetenschappelijke doel is
+		beter begrijpen hoe een zwart gat materiaal afsnoept van een ster die er
+		omheen draait. Het educatieve doel is dat studenten van Nederlandse
+		onderwijsinstellingen van binnenuit meemaken hoe je een ruimtemissie van
+		begin tot eind opbouwt. SRON heeft de leiding over NEBULA – Xplorer,
+		waarmee het bijdraagt aan een nieuwe generatie wetenschappers,
+		ontwerpers en technici voor het Nederlandse ruimteonderzoek.
+	</p>
+	<enhanced:img
+		src="$lib/assets/images/nebula-satellite.png"
+		alt="The Nebula Xplorer Satellite" />
+</section>
 
-  import Mission from "$lib/components/atoms/Mission.svelte";
-  // import NewsComponent from "$lib/components/News-Component/News-Component.svelte";
-</script>
+<style>
+	.section-paragraph-picture {
+		display: grid;
+		gap: 2rem;
+    padding: 3rem 1rem;
 
-<!-- <Mission {data} /> -->
+    @media (min-width: 500px) {
+      padding: 3rem 2rem;
+    }
+
+		@media (min-width: 1000px) {
+      padding: 3rem 3rem;
+			grid-template-columns: 2fr 1fr;
+		}
+
+		h2 {
+			grid-column: 1 / -1;
+		}
+
+		img {
+			max-width: 100%;
+      height: 100%;
+			object-fit: cover;
+		}
+	}
+</style>
