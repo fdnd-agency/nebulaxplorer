0a8171819004f19817edc46379eccbf90c01104e
Iris van Willigen <iris.van.willigen@gmail.com>
2026-02-09 16:30:34 +0100

    feat: add hero component to homepage #298

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script>
-
-  import Mission from "$lib/components/atoms/Mission.svelte";
-  // import NewsComponent from "$lib/components/News-Component/News-Component.svelte";
+  import Hero from '$lib/components/molecules/Hero/Hero.svelte'
+	import heroImg from '$lib/assets/images/grav-waves.jpg?enhanced'
+	import logo from '$lib/assets/logos/SRON_Academy_OnSpaceBlue.svg'
 </script>
 
-<!-- <Mission {data} /> -->
+<Hero
+	titleColor="var(--white)"
+	pageTitle="Nebula Xplorer"
+	background={{ alt: 'Two merging black holes', file: heroImg }}
+	sronIcon={logo} />
