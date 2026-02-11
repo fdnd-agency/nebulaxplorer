9395c3e212203926e9423678d6af3a3b62c20857
Iris van Willigen <iris.van.willigen@gmail.com>
2026-02-11 15:47:45 +0100

    Refactor: unnest unnecessarily nested css #294

--- src/lib/components/NewsComponent/NewsComponent.svelte
+++ src/lib/components/NewsComponent/NewsComponent.svelte
@@ -45,80 +45,80 @@
 		padding-block: 2.5rem;
 		padding-inline: clamp(1rem, 6vw, 3.5rem);
 		border-bottom: var(--cleanroom-100) 0.5rem solid;
+	}
 
-		.news_section_title {
-			color: var(--ultra-140);
-			background-color: var(--white);
-			padding-block: 0.5em 1em;
+	.news_section_title {
+		color: var(--ultra-140);
+		background-color: var(--white);
+		padding-block: 0.5em 1em;
+	}
+
+	.news-grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+		grid-template-rows: fit-content;
+		gap: 2rem;
+		list-style: none;
+	}
+
+	.news-card {
+		position: relative;
+		background-color: var(--white);
+		border-radius: 0.2rem;
+		box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.3);
+		transition:
+			transform 0.3s ease,
+			box-shadow 0.3s ease;
+		align-self: start;
+
+		&:hover {
+			transform: translateY(-0.5rem);
+			box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
 		}
 
-		.news-grid {
-			display: grid;
-			grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-			grid-template-rows: fit-content;
-			gap: 2rem;
-			list-style: none;
+		&:focus-within {
+			transform: translateY(-0.5rem);
+			box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
 		}
+	}
 
-		.news-card {
-			position: relative;
-			background-color: var(--white);
-			border-radius: 0.2rem;
-			box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.3);
-			transition:
-				transform 0.3s ease,
-				box-shadow 0.3s ease;
-			align-self: start;
+	img {
+		width: 100%;
+		height: 15rem;
+		overflow: hidden;
+		margin-top: auto;
 
-			&:hover {
-				transform: translateY(-0.5rem);
-				box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
-			}
+		aspect-ratio: 1 / 1;
+		object-fit: cover;
+		object-position: center;
+	}
 
-			&:focus-within {
-				transform: translateY(-0.5rem);
-				box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
-			}
+	h3 {
+		font-family: var(--heading);
+		font-weight: 400;
+		font-size: 1.3rem;
+		line-height: 1.75rem;
+		color: var(--ultra-140);
+		padding-block: 1rem;
+		padding-inline: clamp(0.75rem, 0.25rem + 1.1vw, 1rem);
+
+		a::after {
+			content: '';
+			position: absolute;
+			left: 0;
+			right: 0;
+			top: 0;
+			bottom: 0;
 		}
+	}
 
-		img {
-			width: 100%;
-			height: 15rem;
-			overflow: hidden;
-			margin-top: auto;
-
-			aspect-ratio: 1 / 1;
-			object-fit: cover;
-			object-position: center;
-		}
-
-		h3 {
-			font-family: var(--heading);
-			font-weight: 400;
-			font-size: 1.3rem;
-			line-height: 1.75rem;
-			color: var(--ultra-140);
-			padding-block: 1rem;
-			padding-inline: clamp(0.75rem, 0.25rem + 1.1vw, 1rem);
-
-			a::after {
-				content: '';
-				position: absolute;
-				left: 0;
-				right: 0;
-				top: 0;
-				bottom: 0;
-			}
-		}
-
-		p {
-			font-family: var(--heading);
-			font-weight: 900;
-			font-size: 1.2rem;
-			color: var(--cleanroom-100);
-			padding-block: 1rem;
-			padding-inline: clamp(0.75rem, 0.25rem + 1.1vw, 1rem);
-		}
+	p {
+		font-family: var(--heading);
+		font-weight: 900;
+		font-size: 1.2rem;
+		color: var(--cleanroom-100);
+		padding-block: 1rem;
+		padding-inline: clamp(0.75rem, 0.25rem + 1.1vw, 1rem);
 	}
 
 	.link-readmore {
