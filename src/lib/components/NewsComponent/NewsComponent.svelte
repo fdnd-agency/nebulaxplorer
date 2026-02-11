131d8d87985a7634442dfbaa34d3d1c28003fc64
Iris van Willigen <iris.van.willigen@gmail.com>
2026-02-11 13:38:56 +0100

    Fix: add explicit height and width to newscard images to reduce CLS #294

--- src/lib/components/NewsComponent/NewsComponent.svelte
+++ src/lib/components/NewsComponent/NewsComponent.svelte
@@ -29,7 +29,11 @@
 	<ul class="news-grid">
 		{#each NewsCards as newscard}
 			<li class="news-card">
-				<img src={newscard.image} alt={newscard.title} />
+				<img
+					src={newscard.image}
+					alt={newscard.title}
+					height="240"
+					width="240" />
 				<h3 class=""><a href={newscard.slug}>{newscard.title}</a></h3>
 				<p>{newscard.type}</p>
 			</li>
