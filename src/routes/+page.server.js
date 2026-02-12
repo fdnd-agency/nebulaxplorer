895122f5bb46533db37323d793927e8b6309c5c5
Iris van Willigen <iris.van.willigen@gmail.com>
2026-02-12 22:54:13 +0100

    Feat: replace newscard section dummy data with dynamic data #301

---
+++ src/routes/+page.server.js
@@ -0,0 +1,10 @@
+import { DIRECTUS_NEWS } from '$env/static/private'
+
+export async function load({ url }) {
+	const limit = 3
+	const newsItemsResponse = await fetch(
+		`${DIRECTUS_NEWS}?sort=-date&limit=${limit}&fields=id,type,title,image`
+	).then((response) => response.json())
+
+	return { newsItems: newsItemsResponse.data }
+}
