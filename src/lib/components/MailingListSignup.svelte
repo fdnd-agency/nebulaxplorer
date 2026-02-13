83eb254e4433c5cd968a7d04e1fd5a277ee919aa
Iris van Willigen <iris.van.willigen@gmail.com>
2026-02-13 16:36:57 +0100

    Feat: add component for mailing list signup #300

---
+++ src/lib/components/MailingListSignup.svelte
@@ -0,0 +1,108 @@
+<section>
+	<div>
+		<h2 class="section_title">Subscribe to our mailing list</h2>
+		<p class="paragraph">
+			to receive news and updates about our research and activities.
+		</p>
+		<form>
+			<label for="email">
+				<span class="visually-hidden">Email</span>
+				<input
+					type="email"
+					id="email"
+					name="email"
+					placeholder="Enter your email address..."
+					required />
+			</label>
+			<button type="submit">Subscribe</button>
+		</form>
+	</div>
+</section>
+
+<style>
+	section {
+		margin-bottom: 3rem;
+	}
+
+	div {
+		max-width: 800px;
+		margin-inline: auto;
+		border: 1px solid var(--cleanroom-100);
+		padding: clamp(1rem, 5vw, 2rem);
+		container-type: inline-size;
+	}
+
+	h2 {
+		color: var(--cleanroom-100);
+		margin-block: 0.25em;
+	}
+
+	p {
+		font-family: var(--paragraph);
+	}
+
+	form {
+		width: 100%;
+		margin-block: 1.5rem;
+
+		display: grid;
+		grid-template-columns: 1fr max-content;
+		grid-template-rows: 3lh;
+		grid-auto-rows: 3lh;
+
+		@container (width < 300px) {
+			grid-template-columns: 1fr;
+			gap: 0.5rem;
+		}
+	}
+
+	label {
+		width: 100%;
+		border: 1px solid white;
+
+		&:focus-within {
+			outline: var(--default-focus);
+			outline-offset: 4px;
+		}
+	}
+
+	input[type='email'] {
+		height: 100%;
+		width: 100%;
+		padding-inline: 1em;
+		background-color: transparent;
+		background: linear-gradient(
+			to right,
+			var(--space-100) 80%,
+			var(--space-160)
+		);
+
+		&::placeholder {
+			color: rgba(255, 255, 255, 0.7);
+		}
+	}
+
+	button {
+		appearance: none;
+		height: 100%;
+		background-color: white;
+		border: 1px solid white;
+		color: black;
+		padding-inline: 1em;
+		transition:
+			background-color 0.2s,
+			color 0.2s;
+
+		&:focus {
+			outline: var(--default-focus);
+			outline-offset: 4px;
+		}
+
+		&:hover,
+		&:focus {
+			background-color: transparent;
+			color: white;
+			border-left: 1px solid white;
+		}
+	}
+</style>
