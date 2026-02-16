2180f370c9ee59bdd05376af524c5f173d42c446
Iris van Willigen <iris.van.willigen@gmail.com>
2026-02-16 15:43:06 +0100

    Refactor: merge the components that had split html and css #258

--- src/lib/components/molecules/AssigmentCards/AssignmentCards.svelte
+++ src/lib/components/molecules/AssigmentCards/AssignmentCards.svelte
@@ -1,5 +1,4 @@
 <script>
-	import './AssignmentCards.css'
 	const { assignments } = $props()
 </script>
 
@@ -27,3 +26,93 @@
 		</li>
 	{/each}
 </ul>
+
+<style>
+	.assignments-header {
+		margin-bottom: 2.5rem;
+
+		h2 {
+			margin-bottom: 0.5rem;
+		}
+
+		p span {
+			color: var(--cleanroom-140);
+		}
+	}
+
+	.assignments-container {
+		display: inherit;
+		grid-template-columns: subgrid;
+		gap: inherit;
+		margin-bottom: 4rem;
+
+		.assignment-card {
+			list-style: none;
+			background: var(--space-100-low-opacity);
+
+			grid-column: 1 / -1;
+
+			@media (min-width: 32rem) {
+				grid-column: span 2;
+			}
+
+			@media (min-width: 56.25rem) {
+				grid-column: span 3;
+			}
+
+			a {
+				display: flex;
+				flex-direction: column;
+				justify-content: space-between;
+				height: 100%;
+				min-height: 11.25rem;
+
+				.assignment-content {
+					padding: 1rem;
+					display: inherit;
+					flex-direction: inherit;
+					justify-content: inherit;
+					height: 100%;
+					color: var(--cleanroom-10);
+
+					.assignment-title {
+						color: var(--cleanroom-140);
+						margin-bottom: 1rem;
+					}
+
+					div {
+						display: inherit;
+						flex-direction: inherit;
+						gap: 0.25rem;
+						p {
+							text-transform: capitalize;
+
+							&:last-child {
+								margin-bottom: 0.5rem;
+							}
+						}
+					}
+
+					/* For the next iteration I recommend changing the capitalization of the values in the fields on Directus instead of capitalizing them with CSS */
+				}
+
+				&:hover {
+					.apply-button {
+						background: var(--ultra-100);
+						color: var(--cleanroom-60);
+					}
+				}
+			}
+		}
+
+		.apply-button {
+			display: inline-block;
+			text-align: center;
+			padding: 1rem;
+			width: 100%;
+			color: var(--space-100);
+			background: var(--cleanroom-100);
+			text-transform: uppercase;
+		}
+	}
+</style>
