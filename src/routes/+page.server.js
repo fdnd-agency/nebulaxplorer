dab9389554d921a04b83f58d3da846c6d3bc6b39
Iris van Willigen <iris.van.willigen@gmail.com>
2026-02-16 10:01:30 +0100

    Refactor: change limit to be hardcoded instead of using a variable #301

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,9 +1,8 @@
 import { DIRECTUS_NEWS } from '$env/static/private'
 
 export async function load({ url }) {
-	const limit = 3
 	const newsItemsResponse = await fetch(
-		`${DIRECTUS_NEWS}?sort=-date&limit=${limit}&fields=id,type,title,image`
+		`${DIRECTUS_NEWS}?sort=-date&limit=3&fields=id,type,title,image`
 	).then((response) => response.json())
 
 	return { newsItems: newsItemsResponse.data ?? [] }
