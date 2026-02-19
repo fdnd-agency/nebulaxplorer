4c4749515adb2c4993159f850c280e6dab56f6f6
Iris van Willigen <iris.van.willigen@gmail.com>
2026-02-19 10:44:55 +0100

    Feat: add basic responsiveness to scientific testimonial with grid #323

--- src/lib/components/molecules/ScientificTestimonial.svelte
+++ src/lib/components/molecules/ScientificTestimonial.svelte
@@ -5,24 +5,44 @@
 </script>
 
 <section>
-	<img src={rushilImage} alt="" />
-	<blockquote>
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nihil
-		inventore pariatur quasi nemo sint dolore optio. Quaerat in, sapiente,
-		reprehenderit quas provident nemo optio autem distinctio, soluta
-		consectetur molestias.
-	</blockquote>
+	<div class="container">
+		<img src={rushilImage} alt="" />
+		<blockquote>
+			Scientific lorem ipsum dolor sit amet consectetur adipisicing elit.
+			Dolorem nihil inventore pariatur quasi nemo sint dolore optio.
+			Quaerat in, sapiente, reprehenderit quas provident nemo optio autem
+			distinctio, soluta consectetur molestias.
+		</blockquote>
+	</div>
 </section>
 
 <style>
 	section {
+		width: 100%;
+	}
+
+	div {
+		margin-inline: auto;
 		display: grid;
-		grid-template-columns: 1fr 2rem 1fr;
-		grid-template-rows: 1fr 2fr 1fr;
+
+		grid-template-columns: 1fr;
+		grid-template-rows: 1fr 1rem 1fr;
+
+		@media (width > 700px) {
+			grid-template-columns: max-content 2rem 1fr;
+			grid-template-rows: 1fr 2fr 1fr;
+		}
 	}
 
 	img {
 		width: 20em;
+		grid-column: 1 / 2;
+		grid-row: 1 / 3;
+
+		@media (width > 700px) {
+			grid-column: 1 / 3;
+			grid-row: 1 / 3;
+		}
 	}
 
 	blockquote {
@@ -30,5 +50,14 @@
 		border-radius: 50px 0;
 		width: 20em;
 		padding: 2rem;
+		margin-inline: 1rem;
+
+		grid-column: 1 / 2;
+		grid-row: 2 / 4;
+
+		@media (width > 700px) {
+			grid-column: 2 / -1;
+			grid-row: 2 / -1;
+		}
 	}
 </style>
