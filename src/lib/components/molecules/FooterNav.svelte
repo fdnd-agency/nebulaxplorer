1ee798cc56e613052cdd385e0b51102d84d2da83
Melissa <melissa.blankenburgh@hva.nl>
2026-02-20 14:46:45 +0100

    refactor: onnodige imports verwijderd uit de aparte footer files

--- src/lib/components/molecules/FooterNav.svelte
+++ src/lib/components/molecules/FooterNav.svelte
@@ -1,14 +1,10 @@
 <script>
 	import {
-		sronLeidenEnhanced as leidenImg,
-		sronGroningenEnhanced as groningenImg,
 		sronAcademyLogoOnSpaceBlue as SRON_Academy_OnSpaceBlue,
 		sronLogoFullWhite as SRON_full_white,
-		nwoIcon as NwoIcon,
 		Instagram as InstagramIcon,
 		LinkedIn as LinkedinIcon,
 		Bsky as BskyIcon,
-		Arrow,
 		Footnote,
 	} from '$lib'
 
--- src/lib/components/organisms/Footer.svelte
+++ src/lib/components/organisms/Footer.svelte
@@ -1,15 +1,6 @@
 <script>
 	// import FooterLocations from '../molecules/FooterLocations.svelte';
 	import {
-		sronLeidenEnhanced as leidenImg,
-		sronGroningenEnhanced as groningenImg,
-		sronAcademyLogoOnSpaceBlue as SRON_Academy_OnSpaceBlue,
-		sronLogoFullWhite as SRON_full_white,
-		nwoIcon as NwoIcon,
-		Instagram as InstagramIcon,
-		LinkedIn as LinkedinIcon,
-		Bsky as BskyIcon,
-		Arrow,
 		FooterLocations,
 		FooterNav,
 	} from '$lib'
