ec6d86106be071b550e3ed92fb7b6d1cbd81ba39
Iris van Willigen <iris.van.willigen@gmail.com>
2026-03-04 09:53:12 +0100

    Style: apply prettier formatting #334

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -8,8 +8,7 @@
 	<title>News</title>
 	<meta
 		name="description"
-		content="The news page features news and resources to inspire the general public and inform the media on the latest discoveries and progress made on the Nebula Xplorer mission."
-	/>
+		content="The news page features news and resources to inspire the general public and inform the media on the latest discoveries and progress made on the Nebula Xplorer mission." />
 </svelte:head>
 
 <Hero pageTitle="News" />
@@ -32,87 +31,85 @@
 <Pagination pagination={data.pagination} />
 
 <style>
- 
-  :global(body) {
-    background-color: var(--ultra-10); 
-  }
+	:global(body) {
+		background-color: var(--ultra-10);
+	}
 
-  .paragraph-block {
-    display: grid;
-    grid-template-columns: subgrid;
-    gap: inherit;
-    row-gap: unset;
- 
-    > *:not(enhanced\:img, img, picture) {
-      grid-column: 1 / -1;
-      max-width: 43.75rem;
-      padding-left: none;
- 
-      @media (min-width: 36.25rem) {
-        grid-column: 1 / 4;
-        padding-right: 1.5rem;
-      }
-      @media (min-width: 56.25rem) {
-        padding-left: 2.5rem;
-        grid-column: 1 / 8;
-      }
-    }
- 
-    .text-title {
-      * + * {
-        margin-top: 0.5rem;
-      }
-    }
- 
-    :global(.text-content),
-    :global(picture) {
-      margin-top: 3rem;
-    }
- 
-    :global(picture) {
-      grid-column: 4 / -1;
-      height: fit-content;
- 
-      display: none;
- 
-      @media (min-width: 36.25rem) {
-        display: block;
-      }
- 
-      @media (min-width: 56.25rem) {
-        grid-column: 8 / -1;
-      }
- 
-      :global(enhanced\:img),
-      :global(img) {
-        width: 100%;
-        object-fit: cover;
-        max-height: 22.8125rem;
-      }
-    }
+	.paragraph-block {
+		display: grid;
+		grid-template-columns: subgrid;
+		gap: inherit;
+		row-gap: unset;
 
-    .heading{
-      padding-bottom: 3rem;
-    }
- 
-    .subtitle,.heading{
-      color: var(--space-140);
-      font-weight: 400;
-    }
+		> *:not(enhanced\:img, img, picture) {
+			grid-column: 1 / -1;
+			max-width: 43.75rem;
+			padding-left: none;
 
-    .subtitle span {
-      color: var(--cleanroom-140);
-    }
-  }
+			@media (min-width: 36.25rem) {
+				grid-column: 1 / 4;
+				padding-right: 1.5rem;
+			}
+			@media (min-width: 56.25rem) {
+				padding-left: 2.5rem;
+				grid-column: 1 / 8;
+			}
+		}
 
- 
-  /* util classes */
-  .orange {
-    color: var(--cleanroom-140);
-  }
- 
-  :global(enhanced\:img) {
-    width: auto;
-  }
+		.text-title {
+			* + * {
+				margin-top: 0.5rem;
+			}
+		}
 
+		:global(.text-content),
+		:global(picture) {
+			margin-top: 3rem;
+		}
+
+		:global(picture) {
+			grid-column: 4 / -1;
+			height: fit-content;
+
+			display: none;
+
+			@media (min-width: 36.25rem) {
+				display: block;
+			}
+
+			@media (min-width: 56.25rem) {
+				grid-column: 8 / -1;
+			}
+
+			:global(enhanced\:img),
+			:global(img) {
+				width: 100%;
+				object-fit: cover;
+				max-height: 22.8125rem;
+			}
+		}
+
+		.heading {
+			padding-bottom: 3rem;
+		}
+
+		.subtitle,
+		.heading {
+			color: var(--space-140);
+			font-weight: 400;
+		}
+
+		.subtitle span {
+			color: var(--cleanroom-140);
+		}
+	}
+
+	/* util classes */
+	.orange {
+		color: var(--cleanroom-140);
+	}
+
+	:global(enhanced\:img) {
+		width: auto;
+	}
 </style>
