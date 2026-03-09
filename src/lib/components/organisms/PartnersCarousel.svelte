55ef6e87f179ec666298a94b09f87d18f93c54c6
Iris van Willigen <iris.van.willigen@gmail.com>
2026-03-09 13:28:19 +0100

    Feat: populate sponsors dummy data object with all sponsors #348

--- src/lib/components/organisms/PartnersCarousel.svelte
+++ src/lib/components/organisms/PartnersCarousel.svelte
@@ -3,43 +3,121 @@
 		{
 			link: '#',
 			image: {
-				src: '/src/lib/assets/images/sponsors/astos.jpg',
-				alt: 'a guy holding a thing',
+				src: '/src/lib/assets/images/sponsors/astos-logo.jpg',
+				alt: 'Astos',
 			},
-			title: 'technology',
 		},
 		{
 			link: '#',
 			image: {
-				src: '/src/lib/assets/images/sponsors/astos.jpg',
-				alt: 'a scientist sciencing',
+				src: '/src/lib/assets/images/sponsors/esatan-logo.png',
+				alt: 'Esatan',
 			},
-			title: 'science',
-		},
-
-		{
-			link: '#',
-			image: {
-				src: '/src/lib/assets/images/sponsors/astos.jpg',
-				alt: 'a guy holding a thing',
-			},
-			title: 'instrumentation',
 		},
 		{
 			link: '#',
 			image: {
-				src: 'src/lib/assets/images/sponsors/astos.jpg',
-				alt: 'a guy holding a thing',
+				src: '/src/lib/assets/images/sponsors/hogeschool-utrecht-logo.png',
+				alt: 'Hogeschool Utrecht',
 			},
-			title: 'placeholder',
 		},
 		{
 			link: '#',
 			image: {
-				src: '/src/lib/assets/images/sponsors/astos.jpg',
-				alt: 'a guy holding a thing',
+				src: '/src/lib/assets/images/sponsors/hogeschool-van-amsterdam-logo.png',
+				alt: 'Hogeschool van Amsterdam',
 			},
-			title: 'lorem ipsum',
+		},
+		{
+			link: '#',
+			image: {
+				src: '/src/lib/assets/images/sponsors/inholland-university-logo.png',
+				alt: 'Inholland University',
+			},
+		},
+		{
+			link: '#',
+			image: {
+				src: '/src/lib/assets/images/sponsors/lis-logo.jpeg',
+				alt: 'Leidse Instrumentmakers School',
+			},
+		},
+		{
+			link: '#',
+			image: {
+				src: '/src/lib/assets/images/sponsors/erasmus-universiteit-rotterdam-logo.png',
+				alt: 'Erasmus Universiteit Rotterdam',
+			},
+		},
+		{
+			link: '#',
+			image: {
+				src: '/src/lib/assets/images/sponsors/mbo-rijnland-logo.png',
+				alt: 'Mbo Rijnland',
+			},
+		},
+		{
+			link: '#',
+			image: {
+				src: '/src/lib/assets/images/sponsors/overdehaagse-logo.png',
+				alt: 'De Haagse Hogeschool',
+			},
+		},
+		{
+			link: '#',
+			image: {
+				src: '/src/lib/assets/images/sponsors/roc-mondriaan-logo.png',
+				alt: 'ROC Mondriaan',
+			},
+		},
+		{
+			link: '#',
+			image: {
+				src: '/src/lib/assets/images/sponsors/roc-nova-logo.png',
+				alt: 'ROC Nova',
+			},
+		},
+		{
+			link: '#',
+			image: {
+				src: '/src/lib/assets/images/sponsors/rugr-logo.gif',
+				alt: 'Rijksuniversiteit Groningen',
+			},
+		},
+		{
+			link: '#',
+			image: {
+				src: '/src/lib/assets/images/sponsors/saxion-logo.jpg',
+				alt: 'Saxion',
+			},
+		},
+		{
+			link: '#',
+			image: {
+				src: '/src/lib/assets/images/sponsors/techniek-college-rotterdam-logo.png',
+				alt: 'Techniek College Rotterdam',
+			},
+		},
+		{
+			link: '#',
+			image: {
+				src: '/src/lib/assets/images/sponsors/tu-delft-logo.png',
+				alt: 'TU Delft',
+			},
+		},
+		{
+			link: '#',
+			image: {
+				src: '/src/lib/assets/images/sponsors/ul-logo.jpg',
+				alt: 'Universiteit Leiden',
+			},
+		},
+		{
+			link: '#',
+			image: {
+				src: '/src/lib/assets/images/sponsors/uva-logo.webp',
+				alt: 'Universiteit van Amsterdam',
+			},
 		},
 	]
 </script>
