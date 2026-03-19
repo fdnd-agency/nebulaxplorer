3762b335340513d55f0eac8f1c2bf9cc021bb753
Iris van Willigen <iris.van.willigen@gmail.com>
2026-03-19 12:57:07 +0100

    Feat: add error handling in frontend #338

--- src/routes/news/[slug]/+page.svelte
+++ src/routes/news/[slug]/+page.svelte
@@ -5,45 +5,52 @@
 </script>
 
 <svelte:head>
-	<title>{data.newsItem[0].title}</title>
-	<meta name="description" content={data.newsItem[0].title} />
+	<title>{data.newsItem[0] ? data.newsItem[0].title : 'News'}</title>
+	<meta
+		name="description"
+		content={data.newsItem[0]
+			? data.newsItem[0].title
+			: 'The news page features news and resources to inspire the general public and inform the media on the latest discoveries and progress made on the Nebula Xplorer mission.'} />
 </svelte:head>
 
 <Hero pageTitle="" />
 
 <Breadcrumb />
 
-<article>
-	<section>
-		<div class="content-container">
-			<h2>{data.newsItem[0].title}</h2>
-			<time datetime={data.newsItem[0].date}>
-				{data.newsItem[0].date}
-			</time>
-			<p>{data.newsItem[0].type}</p>
-			<img
-				src={data.newsItem[0].image
-					? `https://fdnd-agency.directus.app/assets/${data.newsItem[0].image}`
-					: placeholder2}
-				alt="" />
-		</div>
-	</section>
-	<section>
-		<div class="content-container">
-			<h2 class="heading">{data.newsItem[0].title}</h2>
-			<p class="paragraph">
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic
-				praesentium totam molestias possimus aliquid modi similique
-				facilis commodi veritatis, perspiciatis dolore fugit culpa nam
-				et dignissimos expedita accusamus quaerat. Harum. Lorem ipsum
-				dolor sit amet consectetur adipisicing elit. Quas perspiciatis
-				esse sapiente voluptate corrupti consequuntur, cum aut sequi
-				natus repudiandae atque aspernatur ipsa inventore qui velit
-				maxime quae eum. At?
-			</p>
-		</div>
-	</section>
-</article>
+{#if data.newsItem[0]}
+	<article>
+		<section>
+			<div class="content-container">
+				<time datetime={data.newsItem[0].date}>
+					{data.newsItem[0].date}
+				</time>
+				<p>{data.newsItem[0].type}</p>
+				<h1>{data.newsItem[0].title}</h1>
+				<img
+					src={data.newsItem[0].image
+						? `https://fdnd-agency.directus.app/assets/${data.newsItem[0].image}`
+						: placeholder2}
+					alt="" />
+			</div>
+		</section>
+		<section>
+			<div class="content-container">
+				<h2 class="heading">{data.newsItem[0].title}</h2>
+				<p class="paragraph">
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+					Hic praesentium totam molestias possimus aliquid modi
+					similique facilis commodi veritatis, perspiciatis dolore
+					fugit culpa nam et dignissimos expedita accusamus quaerat.
+					Harum. Lorem ipsum dolor sit amet consectetur adipisicing
+					elit. Quas perspiciatis esse sapiente voluptate corrupti
+					consequuntur, cum aut sequi natus repudiandae atque
+					aspernatur ipsa inventore qui velit maxime quae eum. At?
+				</p>
+			</div>
+		</section>
+	</article>
+{:else}
+	<h1>News article not found. Go back to overview?</h1>{/if}
 
 <style>
 	article {
