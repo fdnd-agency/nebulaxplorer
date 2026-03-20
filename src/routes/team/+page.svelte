580e40c5ed7ee44b9931f29ff97422390d54d6e5
Melissa <melissa.blankenburgh@hva.nl>
2026-03-20 10:06:07 +0100

    feat: teams are now grouped by year

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,9 +1,10 @@
 <script module>
-
+/* eslint-disable svelte/require-each-key */
 	import {
 		Hero,
 	} from '$lib'
 	import TeamsComponent from '$lib/components/organisms/TeamsComponent.svelte'
+	// import TeamsYearComponent from '$lib/components/organisms/TeamsYearComponent.svelte'
 
 	const dummydata = [	
 		{
@@ -17,9 +18,31 @@
 			"time_end":"2026-12-30T12:00:00",
 			"time_start":"2026-08-02T12:00:00",
 			"group_photo":"74be76b4-ff0d-4eec-a237-be283887ff99"
+		},{
+			"id":3,
+			"time_end":"2025-06-30T12:00:00",
+			"time_start":"2025-02-02T12:00:00",
+			"group_photo":"74be76b4-ff0d-4eec-a237-be283887ff99"
+		},
+		{
+			"id":4,
+			"time_end":"2025-12-30T12:00:00",
+			"time_start":"2025-08-02T12:00:00",
+			"group_photo":"74be76b4-ff0d-4eec-a237-be283887ff99"
 		},
 
 ]
+
+const groupedByYear = dummydata.reduce((acc, current) => {
+  const year = new Date(current.time_start).getFullYear();
+  if (!acc[year]) {
+    acc[year] = [];
+  }
+  acc[year].push(current);
+  return acc;
+}, {});
+
+const resultArray = Object.entries(groupedByYear);
 
 	
 </script>
@@ -35,15 +58,25 @@
 <Hero titleColor="var(--white)" pageTitle="Team" />
 
 <h2>who are we?</h2>
-<article>
-	{#each dummydata as team(team.id)}
-		<TeamsComponent teams={team}/>
+<section><!-- TODO this article should become its own component (organism?) -->
+	{#each resultArray as year}
+		<h3>{year[0]}</h3>
+
+		<!-- <TeamsYearComponent year={year[1]} -->
+		 
+		<article> <!-- TODO this article should become its own component (molecule?) -->
+			{#each year[1] as teams}
+				<TeamsComponent teams={teams}/>
+			{/each}
+		</article>
+
 	{/each}
-</article>
+</section>
 
 <style>
 	article{
 		display: flex;
 		width: 100%;
+		gap: 2rem;
 	}
 </style>
