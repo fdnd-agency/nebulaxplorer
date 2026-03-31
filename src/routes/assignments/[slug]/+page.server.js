57ee7099ec292e10a46570d284039756d91564ff
Iris van Willigen <iris.van.willigen@gmail.com>
2026-03-31 18:44:40 +0200

    Feat: add assignment fetch #369

---
+++ src/routes/assignments/[slug]/+page.server.js
@@ -0,0 +1,9 @@
+import { DIRECTUS_VACANCIES } from '$env/static/private'
+
+export async function load({ url, params }) {
+	const vacanciesResponse = await fetch(
+		`${DIRECTUS_VACANCIES}/${params.slug}`
+	).then((response) => response.json())
+
+	return { assignment: vacanciesResponse.data ?? [] }
+}
