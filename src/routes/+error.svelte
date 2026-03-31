fbb07bd4abbda6fce930effa19dc69802cac0874
Iris van Willigen <iris.van.willigen@gmail.com>
2026-03-31 16:52:18 +0200

    Feat: display actual error #367

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { Hero, blackhole } from '$lib'
 	import { resolve } from '$app/paths'
+	import { page } from '$app/state'
 </script>
 
 <svelte:head>
@@ -11,7 +12,7 @@
 
 <section>
 	<img src={blackhole} alt="A black hole." />
-	<h1 class="heading">404</h1>
+	<h1 class="heading">{page.status} {page.error.message}</h1>
 	<p class="paragraph">This page may been been absorbed by a black hole.</p>
 	<a href={resolve('/')}>Go back home?</a>
 </section>
@@ -41,8 +42,8 @@
 		}
 	}
 
-	.heading {
-		font-size: 6rem;
+	/* .heading {
+		font-size: 2rem;
 		padding: 0.25em;
-	}
+	} */
 </style>
