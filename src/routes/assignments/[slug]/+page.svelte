50856fabcb9cfa489f7a7a8d6c3b6924b7055ca3
Iris van Willigen <iris.van.willigen@gmail.com>
2026-03-31 18:45:11 +0200

    Feat: display basic data on assignment detail page #369

---
+++ src/routes/assignments/[slug]/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { Breadcrumb, Hero } from '$lib'
+	let { data } = $props()
+	console.log(data)
+</script>
+
+<Hero pageTitle="Assignment" />
+
+<Breadcrumb />
+
+{#if data.assignment.length > 0}
+	<h2>{data.assignment.title}</h2>
+	<p>{data.assignment.location}</p>
+	<p>{data.assignment.organisation}</p>
+
+	<p>{data.assignment.summary}</p>
+	<p>{data.assignment.description}</p>
+{:else}
+	<p>Assignment not found.</p>
+{/if}
