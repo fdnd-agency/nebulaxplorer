ae2c6b23065c29f64aca7a1c17f7840cd919c5ad
Iris van Willigen <iris.van.willigen@gmail.com>
2026-04-01 11:23:06 +0200

    Feat: style data #370

--- src/routes/assignments/[slug]/+page.svelte
+++ src/routes/assignments/[slug]/+page.svelte
@@ -8,12 +8,61 @@
 <Breadcrumb />
 
 {#if data.assignment}
-	<h2>{data.assignment.title}</h2>
-	<p>{data.assignment.location}</p>
-	<p>{data.assignment.organisation}</p>
-
-	<p>{data.assignment.summary}</p>
-	<p>{data.assignment.description}</p>
+	<section class="section-overview">
+		<div class="content-container">
+			<h2 class="section_title">{data.assignment.title}</h2>
+			<ul>
+				<li>{data.assignment.location}</li>
+				<li>{data.assignment.organisation}</li>
+			</ul>
+		</div>
+	</section>
+	<section class="section-details">
+		<div class="content-container">
+			<h3 class="heading">Your Position</h3>
+			<p class="paragraph">{data.assignment.summary}</p>
+			<p class="paragraph">{data.assignment.description}</p>
+		</div>
+	</section>
 {:else}
 	<p>Assignment not found.</p>
+	<a href="/assignments">Go back to overview?</a>
 {/if}
+
+<style>
+	.content-container {
+		padding: 0;
+	}
+
+	section {
+		padding-block: calc(1em + 1vw);
+	}
+
+	ul {
+		display: flex;
+		list-style: none;
+		gap: 1em;
+		margin-block: 1rem;
+	}
+
+	li {
+		text-transform: uppercase;
+		font-family: var(--subtext);
+		color: var(--cleanroom-100);
+	}
+
+	.section-details {
+		background-color: var(--white);
+		color: black;
+		padding-block: calc(1em + 1vw);
+	}
+
+	h3 {
+		color: var(--ultra-100);
+		padding-block: 0.5em;
+	}
+
+	p {
+		margin-block: 0.5em;
+	}
+</style>
