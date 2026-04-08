32c036113fa590aba2ec81330863c1d06bbbdedd
Iris van Willigen <iris.van.willigen@gmail.com>
2026-04-08 14:21:40 +0200

    Fix: temporarily hardcode DIRECTUS_TESTIMONIALS #351

--- src/routes/team/[team]/+page.server.js
+++ src/routes/team/[team]/+page.server.js
@@ -1,8 +1,8 @@
-import { DIRECTUS_TESTIMONIALS } from '$env/static/private'
+// import { DIRECTUS_TESTIMONIALS } from '$env/static/private'
 
 export async function load({ url, params }) {
-	const response = await fetch(`${DIRECTUS_TESTIMONIALS}`).then((response) =>
-		response.json()
-	)
+	const response = await fetch(
+		`https://fdnd-agency.directus.app/items/nebula_xplorer_testimonials`
+	).then((response) => response.json())
 	return { members: response.data ?? [] }
 }
