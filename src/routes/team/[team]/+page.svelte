23be24a00fd91b20b4c3441ce25ed7362a9d7cac
Iris van Willigen <iris.van.willigen@gmail.com>
2026-04-08 14:12:07 +0200

    Feat: allow testimonial cards to grow below 300px #351

--- src/routes/team/[team]/+page.svelte
+++ src/routes/team/[team]/+page.svelte
@@ -61,7 +61,7 @@
 
 	ul {
 		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+		grid-template-columns: repeat(auto-fit, minmax(min(100%, 300px), 1fr));
 		gap: 1rem;
 		margin-block: 1rem 4rem;
 	}
