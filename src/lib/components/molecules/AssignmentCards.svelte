7ae3304e560ed77e2ed1dedf44bcf6a135f21075
Iris van Willigen <iris.van.willigen@gmail.com>
2026-04-10 15:30:53 +0200

    Refactor: refactor HTML & CSS #378

--- src/lib/components/molecules/AssignmentCards.svelte
+++ src/lib/components/molecules/AssignmentCards.svelte
@@ -10,23 +10,23 @@
 			</h2>
 			<p class="heading">
 				Within the NEBULA-Xplorer project there are
-				<span>{assignments.length}</span> assignments available.
+				<span class="orange">{assignments.length}</span> assignments available.
 			</p>
 		</header>
 
 		<ul class="assignments-container">
 			{#each assignments as assignment}
 				<li class="assignment-card">
-					<a href={`/assignments/${assignment.id}`}>
-						<section class="assignment-content">
+					<section class="assignment-content">
+						<a href={`/assignments/${assignment.id}`}>
 							<h3 class="assignment-title">{assignment.title}</h3>
-							<div>
-								<p>{assignment.study_program}</p>
-								<p>{assignment.location}</p>
-							</div>
-						</section>
-						<p class="apply-button supporting">Apply</p>
-					</a>
+						</a>
+						<div>
+							<p>{assignment.study_program}</p>
+							<p>{assignment.location}</p>
+						</div>
+					</section>
+					<p class="apply-button supporting">Apply</p>
 				</li>
 			{/each}
 		</ul>
@@ -40,10 +40,6 @@
 		h2 {
 			margin-bottom: 0.5rem;
 		}
-
-		p span {
-			color: var(--cleanroom-140);
-		}
 	}
 
 	.assignments-container {
@@ -51,68 +47,78 @@
 		grid-template-columns: repeat(auto-fit, minmax(min(100%, 300px), 1fr));
 		gap: 1.25rem;
 		margin-bottom: 4rem;
+	}
 
-		.assignment-card {
-			list-style: none;
-			background: var(--space-100-low-opacity);
+	.assignment-card {
+		list-style: none;
+		background: var(--space-100-low-opacity);
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		height: 100%;
+		min-height: 11.25rem;
 
-			a {
-				display: flex;
-				flex-direction: column;
-				justify-content: space-between;
-				height: 100%;
-				min-height: 11.25rem;
-
-				.assignment-content {
-					padding: 1rem;
-					display: inherit;
-					flex-direction: inherit;
-					justify-content: inherit;
-					height: 100%;
-					color: var(--cleanroom-10);
-
-					.assignment-title {
-						color: var(--cleanroom-140);
-						margin-bottom: 1rem;
-					}
-
-					div {
-						display: inherit;
-						flex-direction: inherit;
-						gap: 0.25rem;
-						p {
-							text-transform: capitalize;
-
-							&:last-child {
-								margin-bottom: 0.5rem;
-							}
-						}
-					}
-
-					/* For the next iteration I recommend changing the capitalization of the values in the fields on Directus instead of capitalizing them with CSS */
-				}
-
-				&:hover {
-					.apply-button {
-						background: var(--ultra-100);
-						color: var(--cleanroom-60);
-					}
-				}
-			}
+		&:focus-within {
+			outline: var(--default-focus);
 		}
 
-		.apply-button {
-			display: inline-block;
-			text-align: center;
-			padding: 1rem;
-			width: 100%;
-			color: var(--space-100);
-			background: var(--cleanroom-100);
-			text-transform: uppercase;
+		&:hover .apply-button,
+		&:focus-within .apply-button {
+			background: var(--ultra-100);
+			color: var(--cleanroom-60);
 		}
 	}
 
+	.assignment-content {
+		padding: 1rem;
+		display: inherit;
+		flex-direction: inherit;
+		justify-content: inherit;
+		height: 100%;
+		color: var(--cleanroom-10);
+		position: relative;
+
+		div {
+			display: inherit;
+			flex-direction: inherit;
+			gap: 0.25rem;
+			padding-block-end: 0.5rem;
+			text-transform: capitalize;
+		}
+
+		/* For the next iteration I recommend changing the capitalization of the values in the fields on Directus instead of capitalizing them with CSS */
+
+		a {
+			outline: none;
+		}
+
+		a::after {
+			content: '';
+			top: 0;
+			bottom: 0;
+			left: 0;
+			right: 0;
+			position: absolute;
+		}
+	}
+
+	.assignment-title {
+		font-size: 1.5rem;
+		color: var(--cleanroom-140);
+		margin-bottom: 1rem;
+	}
+
+	.apply-button {
+		display: inline-block;
+		text-align: center;
+		padding: 1rem;
+		width: 100%;
+		color: var(--space-100);
+		background: var(--cleanroom-100);
+		text-transform: uppercase;
+	}
+
 	.orange {
-		color: var(--cleanroom-100);
+		color: var(--cleanroom-140);
 	}
 </style>
