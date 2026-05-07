9a52ac7070bf228bf1e423b272d47892c33631e6
Iris van Willigen <iris.van.willigen@gmail.com>
2026-05-07 12:27:36 +0200

    Feat: add sponsors to homepage #392

--- src/lib/components/molecules/Sponsors.svelte
+++ src/lib/components/molecules/Sponsors.svelte
@@ -1,11 +1,62 @@
 <script>
-
+	import { logoAstos, logoEsatan } from '$lib'
 </script>
 
 <section>
-    <h3>Our Sponsors</h3>
-    
+	<div class="content-container">
+		<h3 class="subheading">Our Sponsors</h3>
+		<p class="paragraph">
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+			Reprehenderit deleniti rem adipisci ad soluta itaque. Delectus fugit
+			sit cum perspiciatis fugiat, ipsum repellendus cupiditate deserunt
+			laboriosam maxime labore autem eum!
+		</p>
+		<ul>
+			<li>
+				<a href="https://www.astos.de/">
+					<img
+						src={logoAstos}
+						alt="Astos Solutions"
+						height="100"
+						width="150" />
+				</a>
+			</li>
+			<li>
+				<a href="https://www.esatan-tms.com/">
+					<img
+						src={logoEsatan}
+						alt="Esatan-TMS"
+						height="100"
+						width="150" />
+				</a>
+			</li>
+		</ul>
+	</div>
 </section>
-<style>
 
+<style>
+	h3 {
+		margin-block-end: 1rem;
+	}
+
+	ul {
+		display: flex;
+		flex-direction: horizontal;
+		gap: 1rem;
+		flex-wrap: wrap;
+		list-style: none;
+		margin-block: 2rem;
+	}
+
+	img {
+		height: auto;
+		max-height: 10rem;
+		width: auto;
+		max-width: 100%;
+        transition: .3s;
+	}
+
+	a:hover img {
+		transform: scale(1.05);
+	}
 </style>
