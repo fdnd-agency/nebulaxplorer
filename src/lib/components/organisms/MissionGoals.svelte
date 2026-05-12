b00031a23a56a05233f59b5b102c1d704b09786e
Abeeryu <144008500+Abeeryu@users.noreply.github.com>
2026-05-12 17:06:16 +0200

    add dark background
    #390

--- src/lib/components/organisms/MissionGoals.svelte
+++ src/lib/components/organisms/MissionGoals.svelte
@@ -1,4 +1,8 @@
-<section class="goals-block">
+<script>
+	export let colorScheme = 'light'
+</script>
+
+<section class="goals-block" class:dark={colorScheme === 'dark'}>
 	<h2 class="visually-hidden">Mission Goals</h2>
 	<div class="content-container">
 		<div class="goals-grid">
@@ -24,73 +28,77 @@
 </section>
 
 <style>
-.goals-block {
-	background-color: var(--white);
-	color: var(--space-100);
-	padding: 2rem 0;
-}
+	.goals-block {
+		background-color: var(--white);
+		color: var(--space-100);
+		padding: 2rem;
+	}
 
-.goals-grid {
-	display: grid;
-	grid-template-columns: 1fr;
-	gap: 1.5rem;
-}
-
-.goal-box {
-	position: relative;
-	padding: 1.5rem;
-	background: var(--space-100);
-	color: var(--white);
-}
-
-/* orange line */
-.goal-box::before {
-	content: '';
-	position: absolute;
-	top: -1px;
-	left: -1px;
-	width: 40px;
-	height: 2px;
-	background: var(--cleanroom-100);
-}
-
-.visually-hidden h2 {
-    position: absolute;
-    width: 1px;
-    height: 1px;
-    overflow: hidden;
-    clip-path: inset(50%);
-    white-space: nowrap;
-    text-transform: uppercase;
-}
-
-.goal-box h3 {
-	font-size: 0.9rem;
-	font-weight: 500;
-	letter-spacing: 0.2em;
-	color: var(--cleanroom-100);
-	margin-bottom: 0.75rem;
-	text-transform: uppercase;
-}
-
-.goal-box p {
-	color: var(--white);
-	line-height: 1.6;
-}
-
-@media (min-width: 36.25rem) {
 	.goals-grid {
-		grid-template-columns: repeat(2, 1fr);
+		display: grid;
+		grid-template-columns: 1fr;
+		gap: 1.5rem;
 	}
 
 	.goal-box {
-		padding: 2rem;
+		position: relative;
+		padding: 1.5rem;
+		background: var(--space-100);
+		color: var(--white);
 	}
-}
 
-@media (min-width: 56.25rem) {
-	.goals-grid {
-		gap: 2rem;
+	.goals-block.dark {
+		background-color: var(--space-100);
+		color: var(--white);
 	}
-}
+
+	.goals-block.dark .goal-box {
+		color: var(--space-100);
+		border: 1px solid var(--cleanroom-100);
+	}
+
+	.goals-block.dark .goal-box::before {
+		display: none;
+	}
+
+	/* orange line */
+	.goal-box::before {
+		content: '';
+		position: absolute;
+		top: -1px;
+		left: -1px;
+		width: 40px;
+		height: 2px;
+		background: var(--cleanroom-100);
+	}
+
+	.goal-box h3 {
+		font-size: 0.9rem;
+		font-weight: 500;
+		letter-spacing: 0.2em;
+		color: var(--cleanroom-100);
+		margin-bottom: 0.75rem;
+		text-transform: uppercase;
+	}
+
+	.goal-box p {
+		color: var(--white);
+		line-height: 1.6;
+	}
+
+	@media (min-width: 36.25rem) {
+		.goals-grid {
+			grid-template-columns: repeat(2, 1fr);
+		}
+
+		.goal-box {
+			padding: 2rem;
+		}
+	}
+
+	@media (min-width: 56.25rem) {
+		.goals-grid {
+			gap: 2rem;
+		}
+	}
 </style>
--- src/routes/mission/+page.svelte
+++ src/routes/mission/+page.svelte
@@ -146,7 +146,7 @@
 	</div>
 </section>
 
-<MissionGoals/>
+<MissionGoals />
 
 <!-- STEPS BLOCK -->
 
