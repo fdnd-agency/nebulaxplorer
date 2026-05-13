a7ce1573644e8a8ccd7c6c6f7770fb4abc0dbcf9
Abeeryu <144008500+Abeeryu@users.noreply.github.com>
2026-05-13 12:37:04 +0200

    add Prefers Reduced Motion
    #400

--- src/lib/components/molecules/ScrollRocket.svelte
+++ src/lib/components/molecules/ScrollRocket.svelte
@@ -2,28 +2,28 @@
 	import rocket from '$lib/assets/logos/Scroll-rocket.svg'
 	import { ScrollRocket } from '$lib'
 
-	let isScrolling = false;
-    let timer;
+	let isScrolling = false
+	let timer
 
-    function handleScroll() {
-        isScrolling = true;
-        
-        // Restart the timer: If the user stopt with scrolling, then the glow will disappear after 200ms 
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            isScrolling = false;
-        }, 200);
-    }
+	function handleScroll() {
+		isScrolling = true
+
+		// Restart the timer: If the user stopt with scrolling, then the glow will disappear after 200ms
+		clearTimeout(timer)
+		timer = setTimeout(() => {
+			isScrolling = false
+		}, 200)
+	}
 </script>
 
 <svelte:window on:scroll={handleScroll} />
 
 <div class="scroll-track">
-    <div class="scroll-track-line"></div>
-    <div class="scroll-track-fill"></div>
-    <div class="rocket-wrapper" class:is-scrolling={isScrolling}>
-        <img src={rocket} alt="" class="rocket" />
-    </div>
+	<div class="scroll-track-line"></div>
+	<div class="scroll-track-fill"></div>
+	<div class="rocket-wrapper" class:is-scrolling={isScrolling}>
+		<img src={rocket} alt="" class="rocket" />
+	</div>
 </div>
 
 <style>
@@ -81,28 +81,28 @@
 	}
 
 	.rocket-wrapper::after {
-    content: '';
-    position: absolute;
-    bottom: 0;
-    left: 50%;
-    transform: translateX(-50%) translateY(25%);
-    width: 30px;
-    height: 110px;
-    background: radial-gradient(
-        circle, 
-        rgba(221, 91, 74, 0.9) 0%, 
-        rgba(157, 93, 84, 0) 70%
-    );
-    filter: blur(8px);
-    z-index: -1;
-    opacity: 0;
-    transition: opacity 0.2s ease;
-}
+		content: '';
+		position: absolute;
+		bottom: 0;
+		left: 50%;
+		transform: translateX(-50%) translateY(25%);
+		width: 30px;
+		height: 110px;
+		background: radial-gradient(
+			circle,
+			rgba(221, 91, 74, 0.9) 0%,
+			rgba(157, 93, 84, 0) 70%
+		);
+		filter: blur(8px);
+		z-index: -1;
+		opacity: 0;
+		transition: opacity 0.2s ease;
+	}
 
-/* Appears only if the 'is-scrolling' class is present */
-.rocket-wrapper.is-scrolling::after {
-    opacity: 2;
-}
+	/* Appears only if the 'is-scrolling' class is present */
+	.rocket-wrapper.is-scrolling::after {
+		opacity: 2;
+	}
 
 	/* rocket movement */
 	@keyframes rocketMove {
@@ -130,6 +130,23 @@
 
 		@media (min-width: 768px) {
 			width: 40px;
+		}
+	}
+
+	/* Prefers Reduced Motion */
+	@media (prefers-reduced-motion: reduce) {
+		.scroll-track-fill {
+			animation: none;
+			transform: translateX(-50%) scaleY(1);
+		}
+
+		.rocket-wrapper {
+			animation: none;
+			top: 0;
+		}
+
+		.rocket-wrapper::after {
+			display: none;
 		}
 	}
 </style>
