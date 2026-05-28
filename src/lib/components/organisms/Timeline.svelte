b6cfe1cf3560044ef300087fcb2dc286e56b30ab
Iris van Willigen <iris.van.willigen@gmail.com>
2026-05-28 09:58:31 +0200

    Feat: make timeline data dynamic #419

--- src/lib/components/organisms/Timeline.svelte
+++ src/lib/components/organisms/Timeline.svelte
@@ -1,53 +1,56 @@
+<script>
+	import {
+		TimelineStep,
+		placeholder1,
+		placeholder2,
+		sronReviewStep1,
+		rocketLaunchStep5,
+	} from '$lib'
+
+	const data = [
+		{
+			id: 1,
+			title: 'Reviews and Design Confirmation',
+			content:
+				'Before production and launch, the project passes formal ECSS review stages. The System Requirements Review (SRR) defines and validates system needs. The Preliminary Design Review (PDR) evaluates the current design. Finally, the Critical Design Review (CDR) confirms readiness for manufacturing and verifies that all requirements are met.',
+			image: placeholder1,
+		},
+		{
+			id: 2,
+			title: 'Testing and Qualification (V&V)',
+			content:
+				'Verification and Validation ensures that all systems perform correctly. The Engineering Model (EM) is used for functional testing, while the Proto Flight Model (PFM) undergoes strict qualification tests. Activities include COTS component testing, FPGA prototyping, and alignment of the Optical Bench Assembly to withstand launch conditions.',
+			image: placeholder2,
+		},
+		{
+			id: 3,
+			title: 'Assembly, Integration, and Test (AIT)',
+			content:
+				'In the AIT phase, all subsystems are combined into a complete satellite. This includes propulsion and navigation systems such as star trackers. Interface control ensures compatibility between components. The integration of the 15-inch MkII Motorized Lightband connects the satellite securely to the launch vehicle.',
+			image: sronReviewStep1,
+		},
+		{
+			id: 4,
+			title: 'Launch Campaign (Pre-launch Phase)',
+			content:
+				'At the launch site, final preparations are completed. The propulsion system is fueled with propylene and nitrous oxide. The satellite is mounted on a SpaceX Falcon 9 as a rideshare payload. The Flight Readiness Review (FRR) confirms launch readiness before liftoff and the start of LEOP.',
+			image: rocketLaunchStep5,
+		},
+		{
+			id: 5,
+			title: 'The secret fifth step',
+			content: 'Lorem.',
+			image: placeholder1,
+		},
+	]
+</script>
+
 <section>
 	<h3 class="section_title">Mission Timeline</h3>
-	<article>
-		<details name="timeline" open>
-			<summary class="caption">Reviews and Design Confirmation</summary>
-			<p class="paragraph">
-				Before production and launch, the project passes formal ECSS
-				review stages. The System Requirements Review (SRR) defines and
-				validates system needs. The Preliminary Design Review (PDR)
-				evaluates the current design. Finally, the Critical Design
-				Review (CDR) confirms readiness for manufacturing and verifies
-				that all requirements are met.
-			</p>
-		</details>
-		<details name="timeline">
-			<summary class="caption">Testing and Qualification (V&V)</summary>
-			<p class="paragraph">
-				Verification and Validation ensures that all systems perform
-				correctly. The Engineering Model (EM) is used for functional
-				testing, while the Proto Flight Model (PFM) undergoes strict
-				qualification tests. Activities include COTS component testing,
-				FPGA prototyping, and alignment of the Optical Bench Assembly to
-				withstand launch conditions.
-			</p>
-		</details>
-		<details name="timeline">
-			<summary class="caption">
-				Assembly, Integration, and Test (AIT)
-			</summary>
-			<p class="paragraph">
-				In the AIT phase, all subsystems are combined into a complete
-				satellite. This includes propulsion and navigation systems such
-				as star trackers. Interface control ensures compatibility
-				between components. The integration of the 15-inch MkII
-				Motorized Lightband connects the satellite securely to the
-				launch vehicle.
-			</p>
-		</details>
-		<details name="timeline">
-			<summary class="caption">
-				Launch Campaign (Pre-launch Phase)
-			</summary>
-			<p class="paragraph">
-				At the launch site, final preparations are completed. The
-				propulsion system is fueled with propylene and nitrous oxide.
-				The satellite is mounted on a SpaceX Falcon 9 as a rideshare
-				payload. The Flight Readiness Review (FRR) confirms launch
-				readiness before liftoff and the start of LEOP.
-			</p>
-		</details>
+	<article style="--_length: {data.length}">
+		{#each data as step, index (data.id)}
+			<TimelineStep {step} {index} />
+		{/each}
 		<div class="rocket" aria-hidden="true">🚀</div>
 		<div class="background" aria-hidden="true"></div>
 	</article>
@@ -55,7 +58,6 @@
 
 <style>
 	/* Source used: https://css-tricks.com/pure-css-tabs-with-details-grid-and-subgrid/ */
-
 	section {
 		margin-block: calc(2rem + 2vw);
 		container-type: inline-size;
@@ -80,155 +82,12 @@
 		@container (width > 50rem) {
 			display: grid;
 			grid-template-columns: 1fr 2fr;
-			grid-template-rows: repeat(4, 1fr);
+			grid-template-rows: repeat(var(--_length), 1fr);
 			column-gap: 1rem;
 		}
 	}
 
-	details {
-		@container (width > 50rem) {
-			display: grid;
-			grid-template-columns: subgrid;
-			grid-template-rows: subgrid;
-			grid-column: 1 / -1;
-			grid-row: 1 / -1;
-		}
-	}
-
-	details::details-content {
-		display: flex;
-		align-items: flex-end;
-
-		height: 20rem;
-		padding: 1rem;
-
-		background-repeat: no-repeat;
-		background-size: cover;
-
-		opacity: 0;
-		transition:
-			opacity 0.4s,
-			content-visibility 0.4s allow-discrete;
-
-		--space-mid-opacity: hsla(238, 35%, 15%, 0.75);
-		--gradient: linear-gradient(
-			transparent 0%,
-			var(--space-mid-opacity) 30%,
-			var(--space-mid-opacity) 100%
-		);
-
-		@container (width > 50rem) {
-			grid-column: 2;
-			grid-row: 1 / -1;
-			height: auto;
-
-			--gradient: linear-gradient(
-				transparent 0%,
-				transparent 30%,
-				var(--space-mid-opacity) 100%
-			);
-		}
-	}
-
-	details[open]::details-content {
-		opacity: 1;
-	}
-
-	details:not([open])::details-content {
-		display: none;
-
-		@container (width > 50rem) {
-			display: inherit;
-		}
-	}
-
-	summary {
-		grid-column: 1;
-		display: grid;
-		padding: 1rem;
-		cursor: pointer;
-		z-index: 1;
-		position: relative;
-		padding-inline-start: 2em;
-
-		text-transform: uppercase;
-		transition: 0.2s;
-
-		&:hover {
-			background-color: var(--space-60);
-		}
-
-		&::before {
-			content: '';
-			background-image: radial-gradient(white 50%, transparent 50%);
-			position: absolute;
-			align-self: center;
-			transform: scale(1);
-			transition: 1s;
-			transform-origin: center;
-			height: 2rem;
-			width: 2rem;
-			z-index: 2;
-		}
-	}
-
-	p {
-		max-height: 100%;
-		overflow-y: auto;
-		scrollbar-color: var(--space-60) var(--space-100);
-	}
-
-	details[open] summary {
-		font-weight: bold;
-		pointer-events: none; /* ensures there is always one details open */
-
-		&::before {
-			content: '';
-			background-image: radial-gradient(white 50%, transparent 50%);
-			transform: scale(0.1);
-		}
-	}
-
-	/* Set background images for each details element */
-
-	details:nth-of-type(1) summary {
-		grid-row: 1 / span 1;
-	}
-
-	details:nth-of-type(1)::details-content {
-		background-image:
-			var(--gradient), url('$lib/assets/images/placeholder1.jpg');
-	}
-
-	details:nth-of-type(2) summary {
-		grid-row: 2 / span 1;
-	}
-
-	details:nth-of-type(2)::details-content {
-		background-image:
-			var(--gradient), url('$lib/assets/images/placeholder2.jpg');
-	}
-
-	details:nth-of-type(3) summary {
-		grid-row: 3 / span 1;
-	}
-
-	details:nth-of-type(3)::details-content {
-		background-image:
-			var(--gradient), url('$lib/assets/images/sron-review-step-1.jpg');
-	}
-
-	details:nth-of-type(4) summary {
-		grid-row: 4 / span 1;
-	}
-
-	details:nth-of-type(4)::details-content {
-		background-image:
-			var(--gradient), url('$lib/assets/images/rocket-launch-step-5.jpg');
-	}
-
 	/* Decorative rocket timeline */
-
 	.rocket {
 		display: none;
 	}
