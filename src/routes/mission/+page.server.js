7680b7ac40fab2ce197365021185fbb6ef1e1963
Iris van Willigen <iris.van.willigen@gmail.com>
2026-06-07 12:47:22 +0200

    Feat: add error handling #450

--- src/routes/mission/+page.server.js
+++ src/routes/mission/+page.server.js
@@ -1,6 +1,19 @@
 export async function load() {
-	const response = await fetch(
-		'https://fdnd-agency.directus.app/items/nebula_xplorer_timeline'
-	).then((response) => response.json())
-	return { timelineSteps: response.data }
+	// Source: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+	try {
+		const response = await fetch(
+			'https://fdnd-agency.direcus.app/items/nebula_xplorer_timeline'
+		)
+
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`)
+		}
+
+		const responseJson = await response.json()
+
+		return { timelineSteps: responseJson.data }
+	} catch (error) {
+		console.error(error.message)
+		return { timelineSteps: [] }
+	}
 }
