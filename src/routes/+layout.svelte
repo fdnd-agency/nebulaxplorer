47fc672485ecb1b6b69bb00606e156bcc968fc01
Roxy <roxyfokker@hotmail.com>
2026-09-24 15:26:28 +0200

    feat: fonts geïmporteerd in layout #473

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,11 @@
 <script>
+	import '@fontsource/space-grotesk/500.css';
+   	import '@fontsource/space-grotesk/700.css';
+   	import '@fontsource/space-mono/400.css';
+   	import '@fontsource/space-mono/700.css';
+   	import '@fontsource/roboto/400.css';
+   	import '@fontsource/roboto/700.css';
+
 	import favicon from '$lib/assets/favicon.svg';
 	import '$lib/styles/stylesheet.css';
 
